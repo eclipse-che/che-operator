d945527fa444d438e5e37ba618885e5f69dd870a
nickboldt <nboldt@redhat.com>
2019-04-05 15:46:49 -0400

    externalize some variables, set OPERATOR_REPO path, tweak console output
    Change-Id: Iee93957e05133f104c1e1d4c2aa8351e21b305c8
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- e2e/run-okd-tests.sh
+++ e2e/run-okd-tests.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 #
 # Copyright (c) 2012-2018 Red Hat, Inc.
 # This program and the accompanying materials are made
@@ -9,49 +9,57 @@
 #
 # Contributors:
 #   Red Hat, Inc. - initial API and implementation
-set -e
-# download oc
-echo "Download oc client"
+
+OC_VERSION="v3.11.0-0cbc58b"
+GO_TOOLSET_VERSION="1.11.5-3"
+IP_ADDRESS="172.17.0.1"
+
+SCRIPT=$(readlink -f "$0") # this script's absolute path
+SCRIPTPATH=$(dirname "$SCRIPT") # /path/to/e2e/ folder
+if [[ ${WORKSPACE} ]] && [[ -d ${WORKSPACE} ]]; then OPERATOR_REPO=${WORKSPACE}; else OPERATOR_REPO=$(dirname "$SCRIPTPATH"); fi
+
+# download oc client binary
+echo "[INFO] Download oc client"
 mkdir -p ${OPERATOR_REPO}/tmp
 chmod -R 777 ${OPERATOR_REPO}/tmp
-wget https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz -O ${OPERATOR_REPO}/tmp/oc.tar && tar -xvf ${OPERATOR_REPO}/tmp/oc.tar -C ${OPERATOR_REPO}/tmp --strip-components=1
+curl -s -S https://github.com/openshift/origin/releases/download/${OC_VERSION%%-*}/openshift-origin-client-tools-${OC_VERSION}-linux-64bit.tar.gz \
+  -o ${OPERATOR_REPO}/tmp/oc.tar && tar -xvf ${OPERATOR_REPO}/tmp/oc.tar -C ${OPERATOR_REPO}/tmp --strip-components=1
 
 # start OKD
-echo "Starting OKD 3.11"
+echo "[INFO] Start OKD ${OC_VERSION}"
 cd ${OPERATOR_REPO}/tmp
 rm -rf openshift.local.clusterup
-./oc cluster up --public-hostname=172.17.0.1 --routing-suffix=172.17.0.1.nip.io
+./oc cluster up --public-hostname=${IP_ADDRESS} --routing-suffix=${IP_ADDRESS}.nip.io
 ./oc login -u system:admin
 ./oc adm policy add-cluster-role-to-user cluster-admin developer
 ./oc login -u developer -p password
 sleep 10
-echo "Registering a custom resource definition"
+echo "[INFO] Register a custom resource definition"
 ./oc apply -f ${OPERATOR_REPO}/deploy/crds/org_v1_che_crd.yaml
 
 # generate self signed cert
-echo "Generating self signed certificate"
-openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -subj '/CN=*.172.17.0.1.nip.io' -nodes
+echo "[INFO] Generate self signed certificate"
+openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -subj '/CN=*.${IP_ADDRESS}.nip.io' -nodes
 cat cert.pem key.pem > ca.crt
 
 # replace default router cert
-echo "Updating OpenShift router tls secret"
+echo "[INFO] Update OpenShift router tls secret"
 ./oc project default
 ./oc secrets new router-certs tls.crt=ca.crt tls.key=key.pem -o json --type='kubernetes.io/tls' --confirm | ./oc replace -f -
-echo "Initiating a new router deployment"
+echo "[INFO] Initiate a new router deployment"
 sleep 10
 ./oc rollout latest dc/router -n=default
 
-echo "Compiling tests binary"
+echo "[INFO] Compile tests binary"
 docker run -t \
             -v ${OPERATOR_REPO}/tmp:/operator \
-            -v ${OPERATOR_REPO}:/opt/app-root/src/go/src/github.com/eclipse/che-operator registry.access.redhat.com/devtools/go-toolset-rhel7:1.11.5-3 \
+            -v ${OPERATOR_REPO}:/opt/app-root/src/go/src/github.com/eclipse/che-operator registry.access.redhat.com/devtools/go-toolset-rhel7:${GO_TOOLSET_VERSION} \
             sh -c "OOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o /operator/run-tests /opt/app-root/src/go/src/github.com/eclipse/che-operator/e2e/*.go"
 
 cp ${OPERATOR_REPO}/tmp/run-tests ${OPERATOR_REPO}/run-tests
 
+echo "[INFO] Build operator docker image..."
+cd ${OPERATOR_REPO} && docker build -t che/operator -f Dockerfile .
 
-cd ${OPERATOR_REPO}
-echo "Building operator docker image..."
-docker build -t che/operator -f Dockerfile .
-echo "Running tests..."
-./run-tests
+echo "[INFO] Run tests..."
+cd ${OPERATOR_REPO} && ./run-tests
