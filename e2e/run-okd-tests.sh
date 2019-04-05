ab11f670a256b707be5663eb8aed3289d1434e38
Eugene Ivantsov <yivantso@redhat.com>
2019-04-05 16:53:56 +0300

    Fix CI script

--- e2e/run-okd-tests.sh
+++ e2e/run-okd-tests.sh
@@ -12,11 +12,11 @@
 set -e
 # download oc
 echo "Download oc client"
-wget https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz -O /tmp/oc.tar && tar -xvf /tmp/oc.tar -C /tmp --strip-components=1
+mkdir -p ${OPERATOR_REPO}/tmp
+wget https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz -O ${OPERATOR_REPO}/tmp/oc.tar && tar -xvf ${OPERATOR_REPO}/tmp/oc.tar -C ${OPERATOR_REPO}/tmp --strip-components=1
 
 # start OKD
 echo "Starting OKD 3.11"
-mkdir -p ${OPERATOR_REPO}/tmp
 cd ${OPERATOR_REPO}/tmp
 rm -rf openshift.local.clusterup
 ./oc cluster up --public-hostname=172.17.0.1 --routing-suffix=172.17.0.1.nip.io
