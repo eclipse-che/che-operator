4bf6621df113da452b8f0026dbbda5f7de81dae4
David Festal <dfestal@redhat.com>
2019-07-15 15:39:47 +0200

    Manage OLM files (PR #45)
    Implementation of issue https://github.com/eclipse/che/issues/13780
    
    * complete cluster role
    * update operator.yaml
    * Add OLM files for openshift in beta-5 state
    * Add RC 2 release CSV
    * Reordered beta 5 csv in alphabetic order
    * Add first bits of OLM files management
    * lowercase `RC` and remove readiness probe
    * pre-release (with `rc-2.0`) and nightly channels
    * Add the kubernetes version of the OLM package
    * Adding operator sources
    * `OperatorSource`s should be in distinct namespaces
    * Change proposed by @l0rd
    * scripts to update nightly CSVs
    * script to release OLM files
    * Add the script to push OLM files as Quay apps
    * Add script to prepare `community-operators` PRs
    * script to release the operator Go code
    * Rename `*-test-*` to `*-preview-*` and rename the `pre-releases` channel to `stable`
    * `9.9.9` as semver-compliant prefix for nightlies
    
    Signed-off-by: David Festal <dfestal@redhat.com>

---
+++ olm/update-nightly-olm-files.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# Copyright (c) 2012-2018 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Contributors:
+#   Red Hat, Inc. - initial API and implementation
+
+set -e
+
+CURRENT_DIR=$(pwd)
+BASE_DIR=$(cd "$(dirname "$0")"; pwd)
+for platform in 'kubernetes' 'openshift'
+do
+  packageName=eclipse-che-preview-${platform}
+  echo
+  echo "## Updating OperatorHub package '${packageName}' for platform '${platform}'"
+  packageBaseFolderPath=${BASE_DIR}/${packageName}
+  cd "${packageBaseFolderPath}"
+  packageFolderPath="${packageBaseFolderPath}/deploy/olm-catalog/${packageName}"
+  packageFilePath="${packageFolderPath}/${packageName}.package.yaml"
+  lastPackageVersion=$(yq -r '.channels[] | select(.name == "nightly") | .currentCSV' "${packageFilePath}" | sed -e "s/${packageName}.v//")
+  echo "   - Last package version: ${lastPackageVersion}"
+  newNightlyPackageVersion="9.9.9-nightly.$(date +%s)"
+  echo "     => will create a new version: ${newNightlyPackageVersion}"
+  ./build-roles.sh
+  for role in "$(pwd)"/generated/roles/*.yaml
+  do
+    echo "   - Updating new package version with roles defined in: ${role}"
+    cp "$role" generated/current-role.yaml
+    operator-sdk olm-catalog gen-csv --csv-version "${newNightlyPackageVersion}" --from-version="${lastPackageVersion}" 2>&1 | sed -e 's/^/      /'
+  done
+  echo "   - Copying the CRD file"
+  cp "${packageFolderPath}/${lastPackageVersion}/eclipse-che-preview-${platform}.crd.yaml" "${packageFolderPath}/${newNightlyPackageVersion}/eclipse-che-preview-${platform}.crd.yaml"
+  echo "   - Updating the 'nightly' channel with new version in the package descriptor: ${packageFilePath}"
+  echo "     (the previous one is saved with the .old suffix)"
+  sed -e "s/${lastPackageVersion}/${newNightlyPackageVersion}/" "${packageFilePath}" > "${packageFilePath}.new"
+  mv "${packageFilePath}" "${packageFilePath}.old"
+  mv "${packageFilePath}.new" "${packageFilePath}"
+done
+cd "${CURRENT_DIR}"
