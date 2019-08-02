164f562d70c4a0b79ab7aaf48842f6f7adef405b
Florent BENOIT <fbenoit@redhat.com>
2019-08-02 17:37:14 +0200

    docs(olm): information for generating nightlies (#62)
    * docs(olm): information for generating nightlies
    
    Add missing information
    
    Change-Id: Ib9648d16350d0d503a553f5f6a78442f4a2e7141
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

---
+++ olm/README.md
@@ -0,0 +1,31 @@
+# Pre-Requisites
+
+OLM packages scripts are using some required dependencies that need to be installed
+ - [curl](https://curl.haxx.se/)
+ - [https://github.com/kislyuk/yq](https://github.com/kislyuk/yq) and not [http://mikefarah.github.io/yq/](http://mikefarah.github.io/yq/)
+ - [Operator SDK v0.8.2](https://github.com/operator-framework/operator-sdk/blob/v0.8.2/doc/user/install-operator-sdk.md)
+
+If these dependencies are not installed, `docker-run.sh` can be used as a container bootstrap to run a given script with the appropriate dependencies.
+
+Example : `$ docker-run.sh update-nightly-olm-files.sh`
+
+
+# Make new changes to OLM artifacts
+
+Every change needs to be done in a new OLM artifact as previous artifacts are frozen.
+
+A script is generating new folders/files that can be edited.
+
+In `olm` folder
+
+- If all dependencies are installed on the system:
+``` 
+$ update-nightly-olm-files.sh
+```
+
+- To use a docker environment
+``` 
+$ docker-run.sh update-nightly-olm-files.sh
+```
+
+Then the changes can be applied in the newly created CSV files.
---
+++ olm/docker-run.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+#
+# Copyright (c) 2019 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Contributors:
+#   Red Hat, Inc. - initial API and implementation
+
+# git ROOT directory used to mount filesystem
+GIT_ROOT_DIRECTORY=$(git rev-parse --show-toplevel)
+
+# Container image
+IMAGE_NAME="eclipse/che-operator-olm-build"
+
+# Operator SDK
+OPERATOR_SDK_VERSION=v0.8.2
+
+init() {
+  BLUE='\033[1;34m'
+  GREEN='\033[0;32m'
+  RED='\033[0;31m'
+  NC='\033[0m'
+  BOLD='\033[1m'
+}
+
+check() {
+  if [ $# -eq 0 ]; then
+    printf "%bError: %bNo script provided. Command is $ docker-run.sh <script-to-run> [optional-arguments-of-script-to-run]\n" "${RED}" "${NC}"
+    exit 1
+  fi
+  echo "check $1"
+  if [ ! -f "$1" ]; then
+    printf "%bError: %bscript %b provided is not existing. Command is $ docker-run.sh <script-to-run> [optional-arguments-of-script-to-run]\n" "${RED}" "${NC}" "${1}"
+    exit 1
+  fi
+}
+
+# Build image
+build() {
+  printf "%bBuilding image %b${IMAGE_NAME}${NC}..." "${BOLD}" "${BLUE}"
+  if docker build --build-arg OPERATOR_SDK_VERSION=${OPERATOR_SDK_VERSION} -t ${IMAGE_NAME} > docker-build-log 2>&1 -<<EOF
+  FROM evns/yq
+  ARG OPERATOR_SDK_VERSION
+  RUN apk add --no-cache --update curl bash
+  RUN curl -JL https://github.com/operator-framework/operator-sdk/releases/download/${OPERATOR_SDK_VERSION}/operator-sdk-${OPERATOR_SDK_VERSION}-x86_64-linux-gnu -o /bin/operator-sdk && chmod u+x /bin/operator-sdk
+  WORKDIR /workdir/olm
+EOF
+then
+  printf "%b[OK]%b\n" "${GREEN}" "${NC}"
+  rm docker-build-log
+else
+  printf "%bFailure%b\n" "${RED}" "${NC}"
+  cat docker-build-log
+  exit 1
+fi
+}
+
+
+run() {
+  printf "%bRunning%b $*\n" "${BOLD}" "${NC}"
+  if docker run --rm -it -v "${GIT_ROOT_DIRECTORY}":/workdir --entrypoint=/bin/bash ${IMAGE_NAME} "$@"
+  then
+    printf "Script execution %b[OK]%b\n" "${GREEN}" "${NC}"
+  else
+    printf "%bFail to run the script%b\n" "${RED}" "${NC}"
+    exit 1
+  fi
+}
+
+init "$@"
+check "$@"
+build "$@"
+run "$@"
