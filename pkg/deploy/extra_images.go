48d6cefb810386efb4ce4e002e739f32e282adc7
Tom George <tg82490@gmail.com>
2019-10-07 15:49:56 -0500

    Add map of extra images for airgap
    Signed-off-by: Tom George <tg82490@gmail.com>

---
+++ pkg/deploy/extra_images.go
@@ -0,0 +1,7 @@
+package deploy
+
+const (
+	cheWorkspacePluginBrokerInitImage    = "eclipse/che-init-plugin-broker:v0.21"
+	cheWorkspacePluginBrokerUnifiedImage = "eclipse/che-unified-plugin-broker:v0.21"
+	cheServerSecureExposerJwtProxyImage  = "quay.io/eclipse/che-jwtproxy:dbd0578"
+)
