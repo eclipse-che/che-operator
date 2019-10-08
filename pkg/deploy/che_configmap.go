c032558ffc96afbb73ecb9b11a7f72b999462cc5
Tom George <tg82490@gmail.com>
2019-10-08 12:26:20 -0500

    Add a script to generate extra_images.go, only add the extra images data to che configmap if we are in airgap mode
    Signed-off-by: Tom George <tg82490@gmail.com>

--- pkg/deploy/che_configmap.go
+++ pkg/deploy/che_configmap.go
@@ -157,6 +157,8 @@ func GetConfigMapData(cr *orgv1.CheCluster) (cheEnv map[string]string) {
 		CheWebSocketEndpoint:                 wsprotocol + "://" + cheHost + "/api/websocket",
 		WebSocketEndpointMinor:               wsprotocol + "://" + cheHost + "/api/websocket-minor",
 		CheDebugServer:                       cheDebug,
+		CheDockerAlwaysPullImage:             cheDockerAlwaysPullImage,
+		CheWorkspaceSidecarImagePullPolicy:   cheWorkspaceSidecarImagePullPolicy,
 		CheInfrastructureActive:              infra,
 		CheInfraKubernetesServiceAccountName: "che-workspace",
 		BootstrapperBinaryUrl:                protocol + "://" + cheHost + "/agent-binaries/linux_amd64/bootstrapper/bootstrapper",
@@ -237,6 +239,8 @@ func extraImagesConfig(cr *orgv1.CheCluster) map[string]string {
 		"CHE_WORKSPACE_PLUGIN__BROKER_INIT_IMAGE":    patchDefaultImageName(cr, cheWorkspacePluginBrokerInitImage),
 		"CHE_WORKSPACE_PLUGIN__BROKER_UNIFIED_IMAGE": patchDefaultImageName(cr, cheWorkspacePluginBrokerUnifiedImage),
 		"CHE_SERVER_SECURE__EXPOSER_JWTPROXY_IMAGE":  patchDefaultImageName(cr, cheServerSecureExposerJwtProxyImage),
+		"CHE_WORKSPACE_SIDECAR_IMAGE__PULL__POLICY":  cheWorkspaceSidecarImagePullPolicy,
+		"CHE_DOCKER_ALWAYS__PULL__IMAGE":             cheDockerAlwaysPullImage,
 	}
 	return extraImages
 }
--- pkg/deploy/extra_images.go
+++ pkg/deploy/extra_images.go
@@ -5,4 +5,6 @@ const (
 	cheWorkspacePluginBrokerInitImage    = "eclipse/che-init-plugin-broker:v0.21"
 	cheWorkspacePluginBrokerUnifiedImage = "eclipse/che-unified-plugin-broker:v0.21"
 	cheServerSecureExposerJwtProxyImage  = "quay.io/eclipse/che-jwtproxy:dbd0578"
+	cheWorkspaceSidecarImagePullPolicy   = "Always"
+	cheDockerAlwaysPullImage             = "true"
 )
