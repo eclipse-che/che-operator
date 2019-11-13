45da086f93c1437723aa3b510ddd1cd80861dc25
David Festal <dfestal@redhat.com>
2019-11-13 11:29:36 +0100

    Provide easier migration from 1.2 to 2.0 (#119)
    * put back `che` as default for consistency with doc
    * Fix https://issues.jboss.org/browse/CRW-455
    * Complete the cleaning correctly.
    
    Signed-off-by: David Festal <dfestal@redhat.com>

--- pkg/deploy/defaults.go
+++ pkg/deploy/defaults.go
@@ -24,7 +24,7 @@ const (
 	defaultCodeReadyServerImageRepo     = "quay.io/crw/server-rhel8"
 	defaultCheServerImageTag            = "7.3.1"
 	defaultCodeReadyServerImageTag      = "2.0-384"
-	DefaultCheFlavor                    = "codeready"
+	DefaultCheFlavor                    = "che"
 	DefaultChePostgresUser              = "pgche"
 	DefaultChePostgresHostName          = "postgres"
 	DefaultChePostgresPort              = "5432"
@@ -79,6 +79,10 @@ const (
 	OldDefaultPvcJobsUpstreamImageToDetect  = "registry.access.redhat.com/ubi8-minimal:8.0-127"
 	OldDefaultPostgresUpstreamImageToDetect = "centos/postgresql-96-centos7:9.6"
 
+	OldDefaultCodeReadyServerImageRepo="registry.redhat.io/codeready-workspaces/server-operator-rhel8"
+	OldDefaultCodeReadyServerImageTag="1.2"
+	OldCrwPluginRegistryUrl="https://che-plugin-registry.openshift.io"
+	
 	// ConsoleLink default
 	DefaultConsoleLinkName                = "che"
 	DefaultConsoleLinkSection             = "Red Hat Applications"
@@ -86,6 +90,16 @@ const (
 	defaultConsoleLinkUpstreamDisplayName = "Eclipse Che"
 	defaultConsoleLinkDisplayName         = "CodeReady Workspaces"
 )
+
+
+func MigratingToCRW2_0(cr *orgv1.CheCluster) bool {
+	if cr.Spec.Server.CheFlavor == "codeready" &&
+		strings.HasPrefix(cr.Status.CheVersion, "1.2") &&
+		strings.HasPrefix(defaultCodeReadyServerImageTag, "2.0") {
+		return true
+	}
+	return false
+}
 
 func DefaultConsoleLinkDisplayName(cheFlavor string) string {
 	if cheFlavor == "codeready" {
