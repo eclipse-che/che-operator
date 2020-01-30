b8ea0bb27b804bf5919834dd145e215ce415c355
Flavius Lacatusu <flacatus@redhat.com>
2020-01-30 15:05:50 +0100

    Separate ci logic from run locally tests
    Signed-off-by: Flavius Lacatusu <flacatus@redhat.com>

--- .ci/util/ci_common.sh
+++ .ci/util/ci_common.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 #
 # Copyright (c) 2012-2020 Red Hat, Inc.
 # This program and the accompanying materials are made
@@ -10,7 +9,7 @@
 # Contributors:
 #   Red Hat, Inc. - initial API and implementation
 
-set -e
+set -e -x
 
 installStartDocker() {
   if [ -x "$(command -v docker)" ]; then
@@ -30,7 +29,6 @@ install_required_packages() {
     yum -y install epel-release
     # Get all the deps in
     yum -y install libvirt qemu-kvm
- 
   echo '[INFO]CICO: Required virtualization packages installed'
 }
 
@@ -42,10 +40,29 @@ setup_kvm_machine_driver() {
     echo "[INFO] Installing docker machine kvm drivers..."
     curl -L https://github.com/dhiltgen/docker-machine-kvm/releases/download/v0.10.0/docker-machine-driver-kvm-centos7 -o /usr/bin/docker-machine-driver-kvm
     chmod +x /usr/bin/docker-machine-driver-kvm
-
     check_libvirtd=$(systemctl is-active libvirtd)
-    
     if [ $check_libvirtd != 'active' ]; then
         virsh net-start default
     fi
+}
+
+minishift_installation() {
+  echo "[INFO] Downloading Minishift binaries..."
+  curl -s -S -L https://github.com/minishift/minishift/releases/download/v$MSFT_RELEASE/minishift-$MSFT_RELEASE-linux-amd64.tgz \
+    -o ${OPERATOR_REPO}/tmp/minishift-$MSFT_RELEASE-linux-amd64.tar && tar -xvf ${OPERATOR_REPO}/tmp/minishift-$MSFT_RELEASE-linux-amd64.tar -C /usr/bin --strip-components=1
+  echo "[INFO] Sarting a new OC cluster."
+  minishift start --memory=4096 && eval $(minishift oc-env)
+  oc login -u system:admin
+  oc adm policy add-cluster-role-to-user cluster-admin developer && oc login -u developer -p developer
+}
+
+generate_self_signed_certs() {
+    IP_ADDRESS="172.17.0.1"
+    openssl req -x509 \
+                -newkey rsa:4096 \
+                -keyout key.pem \
+                -out cert.pem \
+                -days 365 \
+                -subj "/CN=*.${IP_ADDRESS}.nip.io" \
+                -nodes && cat cert.pem key.pem > ca.crt    
 }
