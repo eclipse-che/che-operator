b1cdcc5d504ef9f0ad8b8f182015b5b109b2cc89
nickboldt <nboldt@redhat.com>
2020-02-28 12:33:37 -0500

    pkg/deploy/extra_images.go no longer exists
    Change-Id: Iafb72511c4725311baecb89fb5eb48ab0239a7a4
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- pkg/deploy/extra_images.go
+++
@@ -1,8 +0,0 @@
-// This file is generated, and contains the latest versions of certain properties from che.properties
-package deploy
-
-const (
-	defaultCheWorkspacePluginBrokerMetadataUpstreamImage  = "quay.io/eclipse/che-plugin-metadata-broker:v3.1.0"
-	defaultCheWorkspacePluginBrokerArtifactsUpstreamImage = "quay.io/eclipse/che-plugin-artifacts-broker:v3.1.0"
-	defaultCheServerSecureExposerJwtProxyUpstreamImage    = "quay.io/eclipse/che-jwtproxy:dbd0578"
-)
