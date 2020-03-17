0221aab001df38218b8d61dbf9b7b93a37c90f67
flacatus <flacatus@redhat.com>
2020-03-17 15:55:25 +0100

    Tests
    Signed-off-by: flacatus <flacatus@redhat.com>

--- .ci/cico_operator_prcheck.sh
+++ .ci/cico_operator_prcheck.sh
@@ -69,12 +69,12 @@ install_minikube() {
   # Download minikube binary
 curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$KUBERNETES_VERSION/bin/linux/amd64/kubectl && \
   chmod +x kubectl &&  \
-sudo mv kubectl /usr/local/bin/
+sudo mv kubectl /usr/bin/
 
 # Download minikube binary
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/$MINIKUBE_VERSION/minikube-linux-amd64 && \
   chmod +x minikube && \
-  sudo mv minikube /usr/local/bin/
+  sudo mv minikube /usr/bin/
 
   sudo -u kubernetes bash -c 'minikube version'
 
