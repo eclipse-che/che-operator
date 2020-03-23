bf1d3550526f8c47432637a9a682145ea92f707a
nickboldt <nboldt@redhat.com>
2020-03-23 19:03:54 -0400

    implement overwriting csv with generated digests and related images; add tags as comments into sources too; support a quieter console mode; if podman not installed fall back to docker
    Change-Id: I47da5adc3db79e64bd595179223a5fee9635343f
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- olm/addDigests.sh
+++ olm/addDigests.sh
@@ -12,8 +12,18 @@
 
 SCRIPTS_DIR=$(cd "$(dirname "$0")"; pwd)
 BASE_DIR="$(pwd)"
-command -v podman >/dev/null 2>&1 || { echo "podman is not installed. Aborting."; exit 1; }
+QUIET=""
+
+PODMAN=$(command -v podman)
+if [[ ! -x $PODMAN ]]; then
+  echo "[WARNING] podman is not installed."
+  PODMAN=$(command -v docker)
+  if [[ ! -x $PODMAN ]]; then
+    echo "[ERROR] docker is not installed. Aborting."; exit 1
+  fi
+fi
 command -v yq >/dev/null 2>&1 || { echo "yq is not installed. Aborting."; exit 1; }
+
 usage () {
 	echo "Usage:   $0 [-w WORKDIR] -s [SOURCE_PATH] -n [csv name] -v [VERSION] "
 	echo "Example: $0 -w $(pwd) -s eclipse-che-preview-openshift/deploy/olm-catalog/eclipse-che-preview-openshift -n eclipse-che-preview-openshift -v 7.9.0"
@@ -28,6 +38,7 @@ while [[ "$#" -gt 0 ]]; do
     '-s') SRC_DIR="$2"; shift 1;;
     '-n') CSV_NAME="$2"; shift 1;;
     '-v') VERSION="$2"; shift 1;;
+    '-q') QUIET="-q"; shift 0;;
 	'--help'|'-h') usage; exit;;
   esac
   shift 1
@@ -40,8 +51,9 @@ mkdir -p ${BASE_DIR}/generated/${CSV_NAME}/
 cp -R ${BASE_DIR}/${SRC_DIR}/* ${BASE_DIR}/generated/${CSV_NAME}/
 
 CSV_FILE="$(find ${BASE_DIR}/generated/${CSV_NAME}/*${VERSION}/ -name "${CSV_NAME}.*${VERSION}.clusterserviceversion.yaml" | tail -1)"; # echo "[INFO] CSV = ${CSV_FILE}"
-${SCRIPTS_DIR}/buildDigestMap.sh -w ${BASE_DIR} -c ${CSV_FILE}
+${SCRIPTS_DIR}/buildDigestMap.sh -w ${BASE_DIR} -c ${CSV_FILE} ${QUIET}
 
+# inject relatedImages block
 names=" "
 count=1
 RELATED_IMAGES='. * { spec : { relatedImages: [ '
@@ -59,9 +71,19 @@ do
   if [ "${names}" != " " ]; then
     RELATED_IMAGES="${RELATED_IMAGES},"
   fi
-  RELATED_IMAGES="${RELATED_IMAGES} { name: \"${name}\", image: \"${dest}\"}"
+  RELATED_IMAGES="${RELATED_IMAGES} { name: \"${name}\", image: \"${dest}\", tag: \"${source}\"}"
   names="${names} ${name} "
 done
 RELATED_IMAGES="${RELATED_IMAGES} ] } }"
 mv ${CSV_FILE} ${CSV_FILE}.old
 yq -Y "$RELATED_IMAGES" ${CSV_FILE}.old > ${CSV_FILE}
+sed -i ${CSV_FILE} -r -e "s|tag: |# tag: |" 
+rm -f ${CSV_FILE}.old
+
+# update original file with generated changes
+CSV_FILE_ORIG=$(find ${BASE_DIR} -name "${CSV_FILE##*/}" | grep -v generated | tail -1)
+mv "${CSV_FILE}" "${CSV_FILE_ORIG}"
+echo "[INFO] CSV updated: ${CSV_FILE_ORIG}"
+
+# cleanup
+rm -fr ${BASE_DIR}/generated
