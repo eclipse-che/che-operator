536885890c83ed1c7681cee57de9c1279e511880
flacatus <flacatus@redhat.com>
2020-03-25 16:57:10 +0100

    FIX
    Signed-off-by: flacatus <flacatus@redhat.com>

--- .ci/cico_olm_prcheck.sh
+++ .ci/cico_olm_prcheck.sh
@@ -27,12 +27,20 @@ init() {
   CHANNEL="stable"
 }
 
+minishift_olm_installation() {
+  MSFT_RELEASE="1.34.2"
+  printInfo "Downloading Minishift binaries"
+  if [ ! -d "$OPERATOR_REPO/tmp" ]; then mkdir -p "$OPERATOR_REPO/tmp" && chmod 777 "$OPERATOR_REPO/tmp"; fi
+  curl -L https://github.com/minishift/minishift/releases/download/v$MSFT_RELEASE/minishift-$MSFT_RELEASE-linux-amd64.tgz \
+    -o ${OPERATOR_REPO}/tmp/minishift-$MSFT_RELEASE-linux-amd64.tar && tar -xvf ${OPERATOR_REPO}/tmp/minishift-$MSFT_RELEASE-linux-amd64.tar -C /usr/bin --strip-components=1
+}
+
 install_Dependencies() {
   installYQ
   install_VirtPackages
   installStartDocker
   setup_kvm_machine_driver
-  minishift_installation
+  minishift_olm_installation
 }
 
 run_olm_tests() {
