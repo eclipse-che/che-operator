2d732905bf9c237323037f308a163e1d196f9d1b
Mykola Morhun <mmorhun@redhat.com>
2020-04-22 17:46:11 +0300

    Fixes according to review
    Signed-off-by: Mykola Morhun <mmorhun@redhat.com>

--- pkg/deploy/job.go
+++ pkg/deploy/job.go
@@ -13,9 +13,12 @@ package deploy
 
 import (
 	"context"
+	"fmt"
 
 	orgv1 "github.com/eclipse/che-operator/pkg/apis/org/v1"
 	"github.com/eclipse/che-operator/pkg/util"
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 	"github.com/sirupsen/logrus"
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
@@ -25,15 +28,14 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 )
 
+var jobDiffOpts = cmp.Options{
+	cmpopts.IgnoreFields(batchv1.Job{}, "TypeMeta", "ObjectMeta", "Status"),
+}
+
 // SyncJobToCluster deploys new instance of given job
 func SyncJobToCluster(instance *orgv1.CheCluster, job *batchv1.Job, clusterAPI ClusterAPI) error {
-	if err := controllerutil.SetControllerReference(instance, job, clusterAPI.Scheme); err != nil {
-		return err
-	}
-
-	jobFound := &batchv1.Job{}
-	err := clusterAPI.Client.Get(context.TODO(), types.NamespacedName{Name: job.Name, Namespace: job.Namespace}, jobFound)
-	if err != nil && errors.IsNotFound(err) {
+	jobFound, err := GetClusterJob(job.ObjectMeta.Name, job.ObjectMeta.Namespace, clusterAPI)
+	if jobFound == nil && err == nil {
 		logrus.Infof("Creating a new object: %s, name: %s", job.Kind, job.Name)
 		err = clusterAPI.Client.Create(context.TODO(), job)
 		if err != nil {
@@ -45,11 +47,30 @@ func SyncJobToCluster(instance *orgv1.CheCluster, job *batchv1.Job, clusterAPI C
 		logrus.Errorf("An error occurred: %s", err)
 		return err
 	}
+
+	diff := cmp.Diff(job, jobFound, jobDiffOpts)
+	if len(diff) > 0 {
+		logrus.Infof("Updating existed object: %s, name: %s", job.Kind, job.Name)
+		fmt.Printf("Difference:\n%s", diff)
+
+		err := clusterAPI.Client.Delete(context.TODO(), jobFound)
+		if err != nil {
+			logrus.Errorf("Failed to update %s %s: %s", jobFound.Name, job.Kind, err)
+			return err
+		}
+
+		err = clusterAPI.Client.Create(context.TODO(), job)
+		if err != nil {
+			logrus.Errorf("Failed to update %s %s: %s", job.Name, job.Kind, err)
+			return err
+		}
+	}
+
 	return nil
 }
 
-// NewJob creates new job configuration by giben parameters.
-func NewJob(checluster *orgv1.CheCluster, name string, namespace string, image string, serviceAccountName string, env map[string]string, backoffLimit int32) *batchv1.Job {
+// GetSpecJob creates new job configuration by giben parameters.
+func GetSpecJob(checluster *orgv1.CheCluster, name string, namespace string, image string, serviceAccountName string, env map[string]string, backoffLimit int32, clusterAPI ClusterAPI) (*batchv1.Job, error) {
 	labels := GetLabels(checluster, util.GetValue(checluster.Spec.Server.CheFlavor, DefaultCheFlavor))
 	labels["component"] = "che-create-tls-secret-job"
 
@@ -60,7 +81,7 @@ func NewJob(checluster *orgv1.CheCluster, name string, namespace string, image s
 		jobEnvVars = append(jobEnvVars, corev1.EnvVar{Name: envVarName, Value: envVarValue})
 	}
 
-	return &batchv1.Job{
+	job := &batchv1.Job{
 		TypeMeta: metav1.TypeMeta{
 			Kind:       "Job",
 			APIVersion: batchv1.SchemeGroupVersion.String(),
@@ -91,4 +112,23 @@ func NewJob(checluster *orgv1.CheCluster, name string, namespace string, image s
 			BackoffLimit: &backoffLimit,
 		},
 	}
+
+	if err := controllerutil.SetControllerReference(checluster, job, clusterAPI.Scheme); err != nil {
+		return nil, err
+	}
+
+	return job, nil
+}
+
+// GetClusterJob gets and returns specified job
+func GetClusterJob(name string, namespace string, clusterAPI ClusterAPI) (*batchv1.Job, error) {
+	job := &batchv1.Job{}
+	err := clusterAPI.Client.Get(context.TODO(), types.NamespacedName{Name: job.Name, Namespace: job.Namespace}, job)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return job, nil
 }
