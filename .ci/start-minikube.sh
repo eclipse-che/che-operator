853731d70075eb092cd6393d9817323bf107ad77
Flavius Lacatusu <flacatus@redhat.com>
2020-05-12 09:20:57 +0200

    Update wrong k8s version

--- .ci/start-minikube.sh
+++ .ci/start-minikube.sh
@@ -12,7 +12,7 @@
 
 # Minikube environments config
 export MINIKUBE_VERSION=v1.8.2
-export KUBERNETES_VERSION=v1.6.2
+export KUBERNETES_VERSION=v1.16.2
 export MINIKUBE_HOME=$HOME
 export CHANGE_MINIKUBE_NONE_USER=true
 export KUBECONFIG=$HOME/.kube/config
