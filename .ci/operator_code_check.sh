bcd623204253f9e68ac601afd4b9af2d156f3c20
flacatus <flacatus@redhat.com>
2020-05-14 14:05:11 +0200

    Fix ci job and adding debug
    Signed-off-by: flacatus <flacatus@redhat.com>

--- .ci/operator_code_check.sh
+++ .ci/operator_code_check.sh
@@ -10,10 +10,10 @@
 # Contributors:
 #   Red Hat, Inc. - initial API and implementation
 
-set -e
+set -e -x
 
 # PR_FILES_CHANGED store all Modified/Created files in Pull Request.
-export PR_FILES_CHANGED=$(git --no-pager diff --name-only HEAD $(git merge-base HEAD master))
+export PR_FILES_CHANGED=$(git --no-pager diff --name-only HEAD $(git merge-base HEAD origin/master))
 
 # transform_files function transform PR_FILES_CHANGED into a new array => FILES_CHANGED_ARRAY.
 function transform_files() {
