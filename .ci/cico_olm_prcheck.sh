3610a7087ecfbe8c059636287e15a42d4e886835
flacatus <flacatus@redhat.com>
2020-05-29 08:44:42 +0200

    Delete logs
    Signed-off-by: flacatus <flacatus@redhat.com>

--- .ci/cico_olm_prcheck.sh
+++ .ci/cico_olm_prcheck.sh
@@ -15,6 +15,10 @@ trap 'Catch_Finish $?' EXIT SIGINT
 # Catch errors and force to delete minikube VM.
 Catch_Finish() {
   rm -rf ~/.kube && yes | minikube delete
+  printInfo "Failed on running tests. Please check logs or contact QE team (e-mail:codereadyqe-workspaces-qe@redhat.com, Slack: #che-qe-internal, Eclipse mattermost: 'Eclipse Che QE'"
+  printInfo "Logs should be availabe on http://artifacts.ci.centos.org/devtools/che/devtools-che-operator-olm-pr-check/${ghprbPullId}/"
+  exit 1
+  archiveArtifacts "che-operator-olm-pr-check"
 }
 
 init() {
