2f3bcd3966ec7b466893c3b3dfce885bb9d75166
Oleksandr Andriienko <oandriie@redhat.com>
2020-06-12 12:59:46 +0300

    Use correct image for e2e tests.
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- .ci/cico_operator_prcheck.sh
+++ .ci/cico_operator_prcheck.sh
@@ -53,7 +53,9 @@ run_tests() {
               sh -c "OOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o /operator/run-tests /opt/app-root/src/go/src/github.com/eclipse/che-operator/e2e/*.go"
   
   printInfo "Build operator docker image and load in to minishift VM..."
-  cd "$OPERATOR_REPO" && docker build -t che/operator -f Dockerfile . && docker save che/operator > operator.tar
+
+  OPERATOR_IMAGE="quay.io/eclipse/che-operator:nightly"
+  cd "$OPERATOR_REPO" && docker build -t "${OPERATOR_IMAGE}" -f Dockerfile . && docker save "${OPERATOR_IMAGE}" > operator.tar
   eval $(minishift docker-env) && docker load -i operator.tar && rm operator.tar
   
   printInfo "Runing e2e tests..."
