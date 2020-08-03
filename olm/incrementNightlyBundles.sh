771fe5e468aea22ecd67f541803982cb376b64b5
Oleksandr Andriienko <oandriie@redhat.com>
2020-08-03 18:04:44 +0300

    Fix up.
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- olm/incrementNightlyBundles.sh
+++ olm/incrementNightlyBundles.sh
@@ -17,25 +17,34 @@ ROOT_PROJECT_DIR=$(dirname "${BASE_DIR}")
 
 source ${BASE_DIR}/check-yq.sh
 
-for platform in 'kubernetes' 'openshift'
-do
-  OPM_BUNDLE_DIR="${ROOT_PROJECT_DIR}/deploy/olm-catalog/che-operator/eclipse-che-preview-${platform}"
-  OPM_BUNDLE_MANIFESTS_DIR="${OPM_BUNDLE_DIR}/manifests"
-  CSV="${OPM_BUNDLE_MANIFESTS_DIR}/che-operator.clusterserviceversion.yaml"
+incrementNightlyVersion() {
+  for platform in 'kubernetes' 'openshift'
+  do
+    OPM_BUNDLE_DIR="${ROOT_PROJECT_DIR}/deploy/olm-catalog/che-operator/eclipse-che-preview-${platform}"
+    OPM_BUNDLE_MANIFESTS_DIR="${OPM_BUNDLE_DIR}/manifests"
+    CSV="${OPM_BUNDLE_MANIFESTS_DIR}/che-operator.clusterserviceversion.yaml"
 
-  currentNightlyVersion=$(yq -r ".spec.version" "${CSV}")
-  echo  "[INFO] current nightly ${platform} version: ${currentNightlyVersion}"
-  #  echo "${currentNightlyVersion}"
-  versionWithoutNightly="${currentNightlyVersion%.nightly}"
-  # echo "${versionWithoutNightly}"
+    currentNightlyVersion=$(yq -r ".spec.version" "${CSV}")
+    echo  "[INFO] current nightly ${platform} version: ${currentNightlyVersion}"
+
+    getNightlyVersionIncrementPart "${currentNightlyVersion}"
+
+    newVersion="${version}-$((incrementPart+1)).nightly"
+
+    echo "[INFO] Set up nightly ${platform} version: ${newVersion}"
+    yq -ryY "(.spec.version) = \"${newVersion}\" | (.metadata.name) = \"eclipse-che-preview-${platform}.v${newVersion}\"" "${CSV}" > "${CSV}.old"
+    mv "${CSV}.old" "${CSV}"
+  done
+}
+
+getNightlyVersionIncrementPart() {
+  nightlyVersion="${1}"
+
+  versionWithoutNightly="${nightlyVersion%.nightly}"
+
   version="${versionWithoutNightly%-*}"
-  # echo "${version}"
+
   incrementPart="${versionWithoutNightly#*-}"
-  # echo "${incrementPart}"
 
-  newVersion="${version}-$((incrementPart+1)).nightly"
-
-  echo "[INFO] Set up nightly ${platform} version: ${newVersion}"
-  yq -ryY "(.spec.version) = \"${newVersion}\" | (.metadata.name) = \"eclipse-che-preview-${platform}.v${newVersion}\"" "${CSV}" > "${CSV}.old"
-  mv "${CSV}.old" "${CSV}"
-done
+  echo "${incrementPart}"
+}
