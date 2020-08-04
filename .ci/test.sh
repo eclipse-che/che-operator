3e9b4f051ebe02881df82ea0c2dbfb8ea0ee9f9d
Oleksandr Andriienko <oandriie@redhat.com>
2020-08-04 17:02:27 +0300

    Fix up.
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

---
+++ .ci/test.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+#
+# Copyright (c) 2012-2020 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Contributors:
+#   Red Hat, Inc. - initial API and implementation
+
+set -e
+go version
+ROOT_PROJECT_DIR="${GITHUB_WORKSPACE}"
+# BASE_DIR="${ROOT_PROJECT_DIR}/olm"
+echo "${GOPATH}"
+# Prevent failing old operator-sdk...
+export GOPATH="/home/runner/work/che-operator/go"
+echo "${GOPATH}"
+
+# install yq
+pip3 install wheel
+pip3 install --upgrade setuptools
+pip3 install yq
+# Make python3 installed modules "visible"
+export PATH=$HOME/.local/bin:$PATH
+ls $HOME/.local/bin
+
+# install operator SDK.
+OPERATOR_SDK_TEMP_DIR="$(mktemp -q -d -t "OPERATOR_SDK_XXXXXX" 2>/dev/null || mktemp -q -d)"
+pushd "${OPERATOR_SDK_TEMP_DIR}" || exit
+echo "[INFO] Downloading 'operator-sdk' cli tool..."
+curl -sLo operator-sdk "$(curl -sL https://api.github.com/repos/operator-framework/operator-sdk/releases/20623716 | jq -r '[.assets[] | select(.name == "operator-sdk-v0.10.1-x86_64-linux-gnu")] | first | .browser_download_url')"
+export OPERATOR_SDK_BINARY="${OPERATOR_SDK_TEMP_DIR}/operator-sdk"
+chmod +x "${OPERATOR_SDK_BINARY}"
+echo "[INFO] Downloading completed!"
+"${OPERATOR_SDK_BINARY}" version
+popd || exit
+
+CSV_FILE_KUBERNETES="deploy/olm-catalog/che-operator/eclipse-che-preview-kubernetes/manifests/che-operator.clusterserviceversion.yaml"
+CRD_FILE_KUBERNETES="deploy/olm-catalog/che-operator/eclipse-che-preview-kubernetes/manifests/org_v1_che_crd.yaml"
+CSV_FILE_OPENSHIFT="deploy/olm-catalog/che-operator/eclipse-che-preview-openshift/manifestsche-operator.clusterserviceversion.yaml"
+CRD_FILE_OPENSHIFT="deploy/olm-catalog/che-operator/eclipse-che-preview-openshift/manifests/org_v1_che_crd.yaml"
+bundle_files=( "${CSV_FILE_KUBERNETES}" "${CRD_FILE_KUBERNETES}" "${CSV_FILE_OPENSHIFT}" "${CRD_FILE_OPENSHIFT}" )
+
+cd ${ROOT_PROJECT_DIR}
+source ${ROOT_PROJECT_DIR}/olm/update-nightly-olm-files.sh
+
+echo "======================================"
+git ls-files -m
+echo "======================================"
+
+IFS=$'\n' read -d '' -r -a changedFiles < <( git ls-files -m ) || true
+echo "Changed files ${changedFiles[*]}"
+
+# git diff
+yq --version
+
+# echo "======================================"
+# ls "${ROOT_PROJECT_DIR}/deploy/olm-catalog/che-operator/eclipse-che-preview-kubernetes/manifests/"
+# echo "======================================"
+
+for file in "${changedFiles[@]}"
+do
+    echo "${file}"
+    echo "======================================"
+    # cat "${ROOT_PROJECT_DIR}/${file}"
+    echo "======================================"
+    # if [[ " ${bundle_files[@]} " =~ " ${file} " ]]; then
+    #   echo "[ERROR] Nightly bundle file should be updated in your pr, please. Use script 'che-operator/olm/update-nightly-olm-files.sh' for this purpose."
+    #   exit 1
+    # fi
+done
