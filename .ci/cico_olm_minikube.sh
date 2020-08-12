3898b94c3d25c0dbc43948a55cec2ac5311189b5
Oleksandr Andriienko <oandriie@redhat.com>
2020-08-13 00:25:19 +0300

    Temp changes
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- .ci/cico_olm_minikube.sh
+++ .ci/cico_olm_minikube.sh
@@ -61,7 +61,7 @@ function run() {
 
 function setPrivateRegistryForDocker {
     dockerDaemonConfig="/etc/docker/daemon.json"
-    sudo touch "${dockerDaemonConfig}"
+    # sudo touch "${dockerDaemonConfig}"
     config='{"insecure-registries" : ["0.0.0.0:5000"]}'
     echo "${config}" | sudo tee "${dockerDaemonConfig}"
 }
