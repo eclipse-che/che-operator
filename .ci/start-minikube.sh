b788806ddf4d09861898570d1f7458cdbde69f52
Oleksandr Andriienko <oandriie@redhat.com>
2020-08-12 18:24:57 +0300

    Temp test changes.
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- .ci/start-minikube.sh
+++ .ci/start-minikube.sh
@@ -111,10 +111,11 @@ echo "[INFO] So proxy pod name is ${REGISTRY_PROXY_POD}"
 echo "[INFO] Ok, let's take a look, what is going on inside registry proxy pod"
 kubectl wait --for=condition=ready "pods/${REGISTRY_PROXY_POD}" --timeout=120s -n "kube-system" || true
 kubectl logs "${REGISTRY_PROXY_POD}" -n kube-system || true
-# docker pull alpine
-# docker tag alpine "${IP}:5000/alpine"
-# docker push "${IP}:5000/alpine"
-# echo "Test push done!"
+echo "[INFO] Push test image...."
+docker pull alpine
+docker tag alpine "${IP}:5000/alpine"
+docker push "${IP}:5000/alpine"
+echo "[INFO] Test push done!"
 
 echo "Minikube start is done!"
 
