8f1baa1c284886b27bb08561fc6f1dafd38d5ae8
Mykhailo Kuznietsov <mkuznets@redhat.com>
2020-08-17 11:54:25 +0300

    Add separate parameter for updating nightly OLM files during release (#405)
    Signed-off-by: Mykhailo Kuznietsov <mkuznets@redhat.com>

--- make-release.sh
+++ make-release.sh
@@ -21,6 +21,7 @@ init() {
   PUSH_GIT_CHANGES=false
   CREATE_PULL_REQUESTS=false
   RELEASE_OLM_FILES=false
+  UPDATE_NIGHTLY_OLM_FILES=false
   RELEASE_DIR=$(cd "$(dirname "$0")"; pwd)
 
   if [[ $# -lt 1 ]]; then usage; exit; fi
@@ -32,6 +33,7 @@ init() {
       '--push-git-changes') PUSH_GIT_CHANGES=true; shift 0;;
       '--pull-requests') CREATE_PULL_REQUESTS=true; shift 0;;
       '--release-olm-files') RELEASE_OLM_FILES=true; shift 0;;
+      '--update-nightly-olm-files') UPDATE_NIGHTLY_OLM_FILES=true; shift 0;;
     '--help'|'-h') usage; exit;;
     esac
     shift 1
@@ -244,7 +246,9 @@ createPRToMasterBranch() {
 run() {
   checkoutToReleaseBranch
   releaseOperatorCode
-  updateNightlyOlmFiles
+  if [[ $UPDATE_NIGHTLY_OLM_FILES == "true" ]]; then
+    updateNightlyOlmFiles
+  fi
   if [[ $RELEASE_OLM_FILES == "true" ]]; then
     releaseOlmFiles
   fi
