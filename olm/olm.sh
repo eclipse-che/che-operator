366ead7a096173e024a4bb85b72fa96ad6b341b4
Oleksandr Andriienko <oandriie@redhat.com>
2020-08-26 08:04:08 +0300

    Improve code.
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- olm/olm.sh
+++ olm/olm.sh
@@ -209,9 +209,9 @@ buildCatalogImage() {
   eval "${OPM_BINARY}" index add \
        --bundles "${CATALOG_BUNDLE_IMAGE_NAME_LOCAL}" \
        --tag "${CATALOG_IMAGENAME}" \
-      #  --pull-tool "${imageTool}" \
-      #  --build-tool "${imageTool}" \
-       --container-tool "${imageTool}" \
+       --pull-tool "${imageTool}" \
+       --build-tool "${imageTool}" \
+      #  --container-tool "${imageTool}" \
        --mode semver \
        "${BUILD_INDEX_IMAGE_ARG}" "${SKIP_TLS_ARG}"
 
