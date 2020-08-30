b7978b6f003f3db833f681efbbb3c08f3d240072
Oleksandr Andriienko <oandriie@redhat.com>
2020-08-31 00:16:29 +0300

    Test
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- olm/testCatalogSource.sh
+++ olm/testCatalogSource.sh
@@ -216,17 +216,16 @@ buildOLMImages() {
 
     cp "${KUBECONFIG}" "$pull_user.kubeconfig" || true
     loginCMD="! oc login --kubeconfig=$pull_user.kubeconfig  --username=${pull_user} --password=${pull_password} > /dev/null"
-    timeout 90s bash -c "${loginCMD}" || return 1
+    timeout 90s bash -c "${loginCMD}" || { echo "Login failed"; return 1; }
     echo "Login done again"
-    echo "Try to get token"
-    token3=$(oc --kubeconfig=$pull_user.kubeconfig whoami -t || true)
-    echo "Token 3 ${token3}"
 
-
-    logInLikeAdmin
+    # logInLikeAdmin
     oc -n "$NAMESPACE" policy add-role-to-user registry-viewer "$pull_user" || true
-
-    
+    echo "Applied policy registry-viewer"
+    echo "Try to get token"
+    token3=$(oc --kubeconfig=$pull_user.kubeconfig whoami -t)
+    echo "Token 3 ${token3}"
+    exit
 
     oc -n "${NAMESPACE}" new-build --binary --strategy=docker --name serverless-bundle
 
