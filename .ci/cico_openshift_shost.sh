878876ae39f90f946e429b6bd4ab0d46e020a8ab
Flavius Lacatusu <flacatus@redhat.com>
2020-10-05 11:48:39 +0200

    Add single host minikube tests
    Signed-off-by: Flavius Lacatusu <flacatus@redhat.com>

--- .ci/cico_openshift_shost.sh
+++ .ci/cico_openshift_shost.sh
@@ -15,33 +15,17 @@ set -ex
 #Stop execution on any error
 trap "catchFinish" EXIT SIGINT
 
-# Catch_Finish is executed after finish script.
-catchFinish() {
-  result=$?
-
-  if [ "$result" != "0" ]; then
-    echo "[ERROR] Please check the openshift ci artifacts"
-    getOCCheClusterLogs
-    exit 1
-  fi
-
-  echo "[INFO] Job finished Successfully.Please check openshift ci artifacts"
-  getOCCheClusterLogs
-
-  exit $result
-}
-
 # Define global environments
 function init() {
   export SCRIPT=$(readlink -f "$0")
   export SCRIPT_DIR=$(dirname "$SCRIPT")
-  export RAM_MEMORY=8192
   export NAMESPACE="che"
   export PLATFORM="openshift"
+  export ARTIFACTS_DIR="/tmp/artifacts/artifacts-che"
 
   # Set operator root directory
   if [[ ${WORKSPACE} ]] && [[ -d ${WORKSPACE} ]]; then
-    export OPERATOR_REPO=${WORKSPACE};
+    export OPERATOR_REPO=${WORKSPgetOCCheClusterLogsACE};
   else
     export OPERATOR_REPO=$(dirname "$SCRIPT_DIR");
   fi
@@ -49,6 +33,29 @@ function init() {
     # CHE_OPERATOR_IMAGE is exposed in openshift ci pod. This image is build in every job and used then to deploy Che
     # More info about how images are builded in Openshift CI: https://github.com/openshift/ci-tools/blob/master/TEMPLATES.md#parameters-available-to-templates
     export OPERATOR_IMAGE=${CHE_OPERATOR_IMAGE}
+    echo "[INFO] Che Operator Image used is: ${CHE_OPERATOR_IMAGE}"
+}
+
+# Catch_Finish is executed after finish script.
+catchFinish() {
+  result=$?
+
+  if [ "$result" != "0" ]; then
+    echo "[ERROR] Please check the openshift ci artifacts"
+    collectCheLogWithChectl
+    exit 1
+  fi
+
+  echo "[INFO] Job finished Successfully.Please check openshift ci artifacts"
+  collectCheLogWithChectl
+
+  exit $result
+}
+
+# Utility to get che events and pod logs from openshift cluster
+function collectCheLogWithChectl() {
+  mkdir -p /tmp/artifacts/artifacts-che
+  chectl server:logs --directory=${ARTIFACTS_DIR}
 }
 
 # Get Token from single host mode deployment
@@ -66,6 +73,7 @@ function waitSingleHostWorkspaceStart() {
   do
     getSingleHostToken
 
+    # List Workspaces and get the status
     chectl workspace:list
     workspaceList=$(chectl workspace:list --chenamespace=${NAMESPACE})
     workspaceStatus=$(echo "$workspaceList" | grep RUNNING | awk '{ print $4} ')
@@ -86,24 +94,6 @@ function waitSingleHostWorkspaceStart() {
   fi
 }
 
-# Utility to get che events and pod logs from openshift cluster
-function getOCCheClusterLogs() {
-  mkdir -p /tmp/artifacts/artifacts-che
-  cd /tmp/artifacts/artifacts-che
-
-  for POD in $(oc get pods -o name -n ${NAMESPACE}); do
-    for CONTAINER in $(oc get -n ${NAMESPACE} ${POD} -o jsonpath="{.spec.containers[*].name}"); do
-      echo ""
-      echo "[INFO] Getting logs from $POD"
-      echo ""
-      oc logs ${POD} -c ${CONTAINER} -n ${NAMESPACE} |tee $(echo ${POD}-${CONTAINER}.log | sed 's|pod/||g')
-    done
-  done
-  echo "[INFO] Get events"
-  oc get events -n ${NAMESPACE}| tee get_events.log
-  oc get all | tee get_all.log
-}
-
 # Deploy Eclipse Che in single host mode
 function run() {
     # Patch file to pass to chectl
@@ -111,7 +101,6 @@ function run() {
 spec:
   server:
     serverExposureStrategy: 'single-host'
-    singleHostExposureType: 'gateway'
   auth:
     updateAdminPassword: false
     openShiftoAuth: false
@@ -120,7 +109,8 @@ EOL
     cat /tmp/che-cr-patch.yaml
 
     # Start to deploy Che
-    chectl server:start --platform=openshift --skip-kubernetes-health-check --installer=operator --chenamespace=${NAMESPACE} --che-operator-cr-patch-yaml=/tmp/che-cr-patch.yaml --che-operator-image=${OPERATOR_IMAGE}
+    chectl server:start --platform=openshift --skip-kubernetes-health-check --installer=operator \
+      --chenamespace=${NAMESPACE} --che-operator-cr-patch-yaml=/tmp/che-cr-patch.yaml --che-operator-image=${OPERATOR_IMAGE}
 
     # Create and start a workspace
     getSingleHostToken # Function from ./util/ci_common.sh
