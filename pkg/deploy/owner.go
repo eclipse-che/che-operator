f249f089c1cff2912e457da67e51a2f4f250f41f
Anatolii Bazko <abazko@redhat.com>
2020-10-08 13:01:20 +0300

    Add missed files
    Signed-off-by: Anatolii Bazko <abazko@redhat.com>

---
+++ pkg/deploy/owner.go
@@ -0,0 +1,44 @@
+//
+// Copyright (c) 2012-2019 Red Hat, Inc.
+// This program and the accompanying materials are made
+// available under the terms of the Eclipse Public License 2.0
+// which is available at https://www.eclipse.org/legal/epl-2.0/
+//
+// SPDX-License-Identifier: EPL-2.0
+//
+// Contributors:
+//   Red Hat, Inc. - initial API and implementation
+//
+package deploy
+
+import (
+	"context"
+	stderrors "errors"
+
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+)
+
+func HasCheClusterOwner(deployContext *DeployContext, object v1.Object) bool {
+	for _, owner := range object.GetOwnerReferences() {
+		if owner.Name == deployContext.CheCluster.Name {
+			return true
+		}
+	}
+
+	return false
+}
+
+func SetCheClusterOwner(deployContext *DeployContext, object v1.Object) error {
+	if err := controllerutil.SetControllerReference(deployContext.CheCluster, object, deployContext.ClusterAPI.Scheme); err != nil {
+		return err
+	}
+
+	robj, ok := object.(runtime.Object)
+	if !ok {
+		return stderrors.New("object " + object.GetName() + " is not a runtime.Object. Cannot update it")
+	}
+
+	return deployContext.ClusterAPI.Client.Update(context.TODO(), robj)
+}
