7bea2f6e216aea73a4d8b074dfdddbcd53d2b331
Michal Vala <mvala@redhat.com>
2020-11-12 08:07:52 +0100

    fix operator-sdk version message (#525)
    Signed-off-by: Michal Vala <mvala@redhat.com>

--- olm/update-crd-files.sh
+++ olm/update-crd-files.sh
@@ -21,7 +21,7 @@ init() {
 
 check() {
   local operatorVersion=$(operator-sdk version)
-  [[ $operatorVersion =~ .*v0.15.2.* ]] || { echo "operator-sdk v0.10.0 is required"; exit 1; }
+  [[ $operatorVersion =~ .*v0.15.2.* ]] || { echo "operator-sdk v0.15.2 is required"; exit 1; }
 }
 
 updateFiles() {
