a1f3f01a0a677443db3799ca51fd91957d154005
Anatolii Bazko <abazko@redhat.com>
2020-12-04 12:49:53 +0200

    Fixes
    Signed-off-by: Anatolii Bazko <abazko@redhat.com>

--- olm/olm.sh
+++ olm/olm.sh
@@ -163,7 +163,7 @@ buildBundleImage() {
 
 # Build catalog source image with index based on bundle image.
 buildCatalogImage() {
-  CATALOG_IMAGENAME=${1}
+  export CATALOG_IMAGENAME=${1}
   if [ -z "${CATALOG_IMAGENAME}" ]; then
     echo "Please specify first argument: catalog image"
     exit 1
