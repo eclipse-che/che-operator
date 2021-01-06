4dddc4aac01fc19601b470995c5b4846d9c3eee0
Anatolii Bazko <abazko@redhat.com>
2021-01-06 10:40:05 +0200

    Add liveness and readiness probes
    Signed-off-by: Anatolii Bazko <abazko@redhat.com>

---
+++ .github/bin/minikube/test-latest-update.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# Copyright (c) 2020 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Contributors:
+#   Red Hat, Inc. - initial API and implementation
+
+set -e
+set -x
+
+# Get absolute path for root repo directory from github actions context: https://docs.github.com/en/free-pro-team@latest/actions/reference/context-and-expression-syntax-for-github-actions
+export OPERATOR_REPO="${GITHUB_WORKSPACE}"
+source "${OPERATOR_REPO}"/.github/bin/common.sh
+
+# Stop execution on any error
+trap "catchFinish" EXIT SIGINT
+
+runTest() {
+  deployEclipseChe "operator" "minikube" "quay.io/eclipse/che-operator:${LAST_PACKAGE_VERSION}" ${LAST_OPERATOR_TEMPLATE}
+  createWorkspace
+
+  updateEclipseChe  ${OPERATOR_IMAGE} ${TEMPLATES}
+  waitEclipseCheDeployed ${LAST_PACKAGE_VERSION}
+
+  startExistedWorkspace
+  waitWorkspaceStart
+}
+
+init
+initLatestTemplates
+initStableTemplates "kubernetes" "stable"
+buildCheOperatorImage
+copyCheOperatorImageToMinikube
+runTest
--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -35,6 +35,7 @@ import (
 	sdkVersion "github.com/operator-framework/operator-sdk/version"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/manager/signals"
 )
@@ -103,9 +104,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Become the leader before proceeding
-	leader.Become(context.TODO(), "che-operator-lock")
-
 	r := ready.NewFileReady()
 	err = r.Set()
 	if err != nil {
@@ -114,10 +112,15 @@ func main() {
 	}
 	defer r.Unset()
 
+	// Become the leader before proceeding
+	leader.Become(context.TODO(), "che-operator-lock")
+
 	// Create a new Cmd to provide shared dependencies and start components
 	options := manager.Options{
-		Namespace: namespace,
+		Namespace:              namespace,
+		HealthProbeBindAddress: ":6789",
 	}
+
 	mgr, err := manager.New(cfg, options)
 	if err != nil {
 		log.Error(err, "")
@@ -155,6 +158,12 @@ func main() {
 	// Setup all Controllers
 	if err := controller.AddToManager(mgr); err != nil {
 		log.Error(err, "")
+		os.Exit(1)
+	}
+
+	// Setup health checks
+	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
+		log.Error(err, "Unable to set up health check")
 		os.Exit(1)
 	}
 
