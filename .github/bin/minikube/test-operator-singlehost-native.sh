aa52e2892bf08dc167c31a929fb5c5c1c816e86d
Anatolii Bazko <abazko@redhat.com>
2021-01-08 18:30:58 +0200

    Add liveness and readiness probes (#610)
    * Add liveness and readiness probes
    
    Signed-off-by: Anatolii Bazko <abazko@redhat.com>

--- .github/bin/minikube/test-operator-singlehost-native.sh
+++ .github/bin/minikube/test-operator-singlehost-native.sh
@@ -21,7 +21,6 @@ source "${OPERATOR_REPO}"/.github/bin/common.sh
 trap "catchFinish" EXIT SIGINT
 
 prepareTemplates() {
-  disableOpenShiftOAuth ${TEMPLATES}
   disableUpdateAdminPassword ${TEMPLATES}
   setCustomOperatorImage ${TEMPLATES} ${OPERATOR_IMAGE}
   setServerExposureStrategy ${TEMPLATES} "single-host"
