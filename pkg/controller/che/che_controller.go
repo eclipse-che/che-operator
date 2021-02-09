7fb00902d692d00150c26bde28bf63a7575274cf
Oleksandr Andriienko <oandriie@redhat.com>
2021-02-09 17:48:19 +0200

    Update pkg/controller/che/che_controller.go
    Co-authored-by: Anatolii Bazko <abazko@redhat.com>

--- pkg/controller/che/che_controller.go
+++ pkg/controller/che/che_controller.go
@@ -1135,7 +1135,7 @@ func (r *ReconcileChe) autoEnableOAuth(deployContext *deploy.DeployContext, requ
 				oauth = true
 			} else if util.IsInitialOpenShiftOAuthUserEnabled(cr) {
 				if err := r.userHandler.CreateOAuthInitialUser(deploy.DefaultCheFlavor(cr), cr.Namespace, openshitOAuth); err != nil {
-					message = warningNoIdentityProvidersMessage + " Operator tried to create initial identity provider, but failed. Cause: " + err.Error()
+					message = warningNoIdentityProvidersMessage + " Operator tried to create initial OpenShift OAuth user for HTPasswd identity provider, but failed. Cause: " + err.Error()
 					logrus.Error(message)
 					logrus.Info(" You can create identity provider manually:" + howToAddIdentityProviderLinkOS4)
 					reason = failedNoIdentityProviders
