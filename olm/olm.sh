85864e9f1cb6e85ce8f3244b30b5b29c4e8a462a
Oleksandr Andriienko <oandriie@redhat.com>
2021-03-01 20:21:14 +0200

    Update olm bundle
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- olm/olm.sh
+++ olm/olm.sh
@@ -311,21 +311,10 @@ installOperatorMarketPlace() {
   echo "[INFO] Installing test pre-requisistes"
   IFS=$'\n' read -d '' -r -a olmApiGroups < <( kubectl api-resources --api-group=operators.coreos.com -o name ) || true
   if [ -z "${olmApiGroups[*]}" ]; then
-      OLM_VERSION=0.15.1
-      ## todo use newer and stable OLM 0.17.1!...
-      MARKETPLACE_VERSION=4.5
-      OPERATOR_MARKETPLACE_VERSION="release-${MARKETPLACE_VERSION}"
-      curl -sL https://github.com/operator-framework/operator-lifecycle-manager/releases/download/${OLM_VERSION}/install.sh | bash -s ${OLM_VERSION}
-      kubectl apply -f https://raw.githubusercontent.com/operator-framework/operator-marketplace/${OPERATOR_MARKETPLACE_VERSION}/deploy/upstream/01_namespace.yaml
-      kubectl apply -f https://raw.githubusercontent.com/operator-framework/operator-marketplace/${OPERATOR_MARKETPLACE_VERSION}/deploy/upstream/03_operatorsource.crd.yaml
-      kubectl apply -f https://raw.githubusercontent.com/operator-framework/operator-marketplace/${OPERATOR_MARKETPLACE_VERSION}/deploy/upstream/04_service_account.yaml
-      kubectl apply -f https://raw.githubusercontent.com/operator-framework/operator-marketplace/${OPERATOR_MARKETPLACE_VERSION}/deploy/upstream/05_role.yaml
-      kubectl apply -f https://raw.githubusercontent.com/operator-framework/operator-marketplace/${OPERATOR_MARKETPLACE_VERSION}/deploy/upstream/06_role_binding.yaml
-      sleep 1
-      kubectl apply -f https://raw.githubusercontent.com/operator-framework/operator-marketplace/${OPERATOR_MARKETPLACE_VERSION}/deploy/upstream/07_upstream_operatorsource.cr.yaml
-      curl -sL https://raw.githubusercontent.com/operator-framework/operator-marketplace/${OPERATOR_MARKETPLACE_VERSION}/deploy/upstream/08_operator.yaml | \
-      sed -e "s;quay.io/openshift/origin-operator-marketplace:latest;quay.io/openshift/origin-operator-marketplace:${MARKETPLACE_VERSION};" | \
-      kubectl apply -f -
+      OLM_VERSION=v0.17.0
+      curl -L https://github.com/operator-framework/operator-lifecycle-manager/releases/download/${OLM_VERSION}/install.sh -o install.sh | bash -s ${OLM_VERSION}
+      # chmod +x install.sh
+      # ./install.sh v0.17.0
   fi
 }
 
