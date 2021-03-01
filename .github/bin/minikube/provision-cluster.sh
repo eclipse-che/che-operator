e3f0a7b3747517458de641f3e6252556e25102c6
Oleksandr Andriienko <oandriie@redhat.com>
2021-03-02 01:27:48 +0200

    Test.
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- .github/bin/minikube/provision-cluster.sh
+++ .github/bin/minikube/provision-cluster.sh
@@ -45,10 +45,10 @@ minikube config set vm-driver none
 minikube version
 
 # minikube start
-sudo bash -c "export CHANGE_MINIKUBE_NONE_USER=true; minikube start --driver=none --kubernetes-version=$KUBERNETES_VERSION --extra-config=kubelet.resolv-conf=/run/systemd/resolve/resolv.conf --memory=6400"
+# sudo bash -c "export CHANGE_MINIKUBE_NONE_USER=true; minikube start --driver=none --kubernetes-version=$KUBERNETES_VERSION --extra-config=kubelet.resolv-conf=/run/systemd/resolve/resolv.conf --memory=6400"
 # sudo minikube stop
-sudo minikube update-context
-# minikube start
+sudo minikube update-context default
+minikube start
 
 #Give god access to the k8s API
 kubectl apply -f - <<EOF
