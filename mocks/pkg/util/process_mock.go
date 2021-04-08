95681b27606287e57a0b412748ba695c36454444
Anatolii Bazko <abazko@redhat.com>
2021-04-08 17:51:07 +0300

    Fixes
    Signed-off-by: Anatolii Bazko <abazko@redhat.com>

--- mocks/pkg/util/process_mock.go
+++ mocks/pkg/util/process_mock.go
@@ -5,8 +5,9 @@
 package mock_util
 
 import (
-	gomock "github.com/golang/mock/gomock"
 	reflect "reflect"
+
+	gomock "github.com/golang/mock/gomock"
 )
 
 // MockRunnable is a mock of Runnable interface
