1229c0c05e0c452a88176ba02507e80907747b2e
Anatolii Bazko <abazko@redhat.com>
2021-04-20 10:17:23 +0300

    Use CRD v1 of DWCO resources
    Signed-off-by: Anatolii Bazko <abazko@redhat.com>

--- pkg/deploy/dev-workspace/dev_workspace.go
+++ pkg/deploy/dev-workspace/dev_workspace.go
@@ -24,7 +24,6 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
 	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
-	apiextensionsv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
@@ -319,7 +318,7 @@ func syncDwCheRoleBinding(deployContext *deploy.DeployContext) (bool, error) {
 }
 
 func syncDwCheCRD(deployContext *deploy.DeployContext) (bool, error) {
-	return syncObject(deployContext, DevWorkspaceCheManagersCRDFile, &apiextensionsv1beta1.CustomResourceDefinition{})
+	return syncObject(deployContext, DevWorkspaceCheManagersCRDFile, &apiextensionsv1.CustomResourceDefinition{})
 }
 
 func syncDwCheConfigMap(deployContext *deploy.DeployContext) (bool, error) {
