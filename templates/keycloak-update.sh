a62e44a20cfcf2342870d527b35e498d0767d607
Max Shaposhnik <mshaposh@redhat.com>
2021-04-20 09:11:35 +0300

    Add update script

---
+++ templates/keycloak-update.sh
@@ -0,0 +1,34 @@
+#
+# Copyright (c) 2020 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Contributors:
+#   Red Hat, Inc. - initial API and implementation
+#
+
+connectToKeycloak() {
+  {{ .Script }} config credentials --server http://0.0.0.0:8080/auth --realm master --user {{ .KeycloakAdminUserName }} --password {{ .KeycloakAdminPassword }}
+}
+
+updateKeycloak() {
+  {{ .Script }} update clients \
+    -r '{{ .KeycloakRealm }}' \
+    -s clientId={{ .KeycloakClientId }} \
+    -s redirectUris='["http://{{ .CheHost }}/dashboard/*", "https://{{ .CheHost }}/dashboard/*", "http://{{ .CheHost }}/factory*", "https://{{ .CheHost }}/factory*", "http://{{ .CheHost }}/f*", "https://{{ .CheHost }}/f*", "http://{{ .CheHost }}/_app/*", "https://{{ .CheHost }}/_app/*", "http://{{ .CheHost }}/swagger/*", "https://{{ .CheHost }}/swagger/*"]' \
+}
+
+checkKeycloak() {
+  REDIRECT_URIS=$({{ .Script }} get clients/{{ .KeycloakClientId }} -r '{{ .KeycloakRealm }}' | jq '.redirectUris')
+  FIND="http://{{ .CheHost }}/factory*"
+  for URI in "${REDIRECT_URIS[@]}"; do
+    [[ $FIND == "$URI" ]] && exit 1;
+  done
+}
+
+connectToKeycloak
+checkKeycloak
+updateKeycloak
