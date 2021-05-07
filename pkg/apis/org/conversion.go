1f136cab683aacf031973c380d1d4bd7306a2316
Lukas Krejci <lkrejci@redhat.com>
2021-05-07 14:24:13 +0200

    Implements eclipse/che#19846.
    Defines a new version of the CheCluster custom resource and implements
    conversion functions from and to v1. The v2alpha1 is intentionally NOT
    part of the CRD yet and is meant to be used only at runtime by
    the operator code.

---
+++ pkg/apis/org/conversion.go
@@ -0,0 +1,193 @@
+package org
+
+import (
+	v1 "github.com/eclipse-che/che-operator/pkg/apis/org/v1"
+	"github.com/eclipse-che/che-operator/pkg/apis/org/v2alpha1"
+	"github.com/eclipse-che/che-operator/pkg/util"
+	"gopkg.in/yaml.v2"
+)
+
+const (
+	v1StorageAnnotation          = "che.eclipse.org/cheClusterV1Spec"
+	v2alpha1StorageAnnotation    = "che.eclipse.org/cheClusterV2alpha1Spec"
+	routeDomainSuffixPropertyKey = "CHE_INFRA_OPENSHIFT_ROUTE_HOST_DOMAIN__SUFFIX"
+)
+
+func V1ToV2alpha1(v1 *v1.CheCluster, v2 *v2alpha1.CheCluster) error {
+	v2Data := v1.Annotations[v2alpha1StorageAnnotation]
+	v2Spec := v2alpha1.CheClusterSpec{}
+	if v2Data != "" {
+		err := yaml.Unmarshal([]byte(v2Data), &v2Spec)
+		if err != nil {
+			return err
+		}
+	}
+
+	v2.ObjectMeta = v1.ObjectMeta
+	v2.Spec = v2Spec
+
+	v1Spec, err := yaml.Marshal(v1.Spec)
+	if err != nil {
+		return err
+	}
+	if v2.Annotations == nil {
+		v2.Annotations = map[string]string{}
+	}
+	v2.Annotations[v1StorageAnnotation] = string(v1Spec)
+
+	v1ToV2alpha1_Host(v1, v2)
+	v1ToV2alpha1_WorkspaceBaseDomain(v1, v2)
+	v1ToV2alpha1_GatewayEnabled(v1, v2)
+	v1ToV2alpha1_GatewayImage(v1, v2)
+	v1ToV2alpha1_GatewayConfigurerImage(v1, v2)
+	v1ToV2alpha1_TlsSecretName(v1, v2)
+	v1ToV2alpha1_K8sIngressAnnotations(v1, v2)
+
+	return nil
+}
+
+func V2alpha1ToV1(v2 *v2alpha1.CheCluster, v1Obj *v1.CheCluster) error {
+	v1Data := v2.Annotations[v1StorageAnnotation]
+	v1Spec := v1.CheClusterSpec{}
+	if v1Data != "" {
+		err := yaml.Unmarshal([]byte(v1Data), &v1Spec)
+		if err != nil {
+			return err
+		}
+	}
+
+	v1Obj.ObjectMeta = v2.ObjectMeta
+	v1Obj.Spec = v1Spec
+	v1Obj.Status = v1.CheClusterStatus{}
+
+	v2Spec, err := yaml.Marshal(v2.Spec)
+	if err != nil {
+		return err
+	}
+	if v1Obj.Annotations == nil {
+		v1Obj.Annotations = map[string]string{}
+	}
+	v1Obj.Annotations[v2alpha1StorageAnnotation] = string(v2Spec)
+
+	v2alpha1ToV1_Host(v1Obj, v2)
+	v2alpha1ToV1_WorkspaceBaseDomain(v1Obj, v2)
+	v2alpha1ToV1_GatewayEnabled(v1Obj, v2)
+	v2alpha1ToV1_GatewayImage(v1Obj, v2)
+	v2alpha1ToV1_GatewayConfigurerImage(v1Obj, v2)
+	v2alpha1ToV1_TlsSecretName(v1Obj, v2)
+	v2alpha1ToV1_K8sIngressAnnotations(v1Obj, v2)
+
+	return nil
+}
+
+func v1ToV2alpha1_Host(v1 *v1.CheCluster, v2 *v2alpha1.CheCluster) {
+	v2.Spec.Gateway.Host = v1.Spec.Server.CheHost
+}
+
+func v1ToV2alpha1_WorkspaceBaseDomain(v1 *v1.CheCluster, v2 *v2alpha1.CheCluster) {
+	if util.IsOpenShift {
+		v2.Spec.WorkspaceBaseDomain = v1.Spec.Server.CustomCheProperties[routeDomainSuffixPropertyKey]
+	} else {
+		v2.Spec.WorkspaceBaseDomain = v1.Spec.K8s.IngressDomain
+	}
+}
+
+func v1ToV2alpha1_GatewayEnabled(v1 *v1.CheCluster, v2 *v2alpha1.CheCluster) {
+	val := util.GetServerExposureStrategy(v1) == "single-host"
+	v2.Spec.Gateway.Enabled = &val
+}
+
+func v1ToV2alpha1_GatewayImage(v1 *v1.CheCluster, v2 *v2alpha1.CheCluster) {
+	v2.Spec.Gateway.Image = v1.Spec.Server.SingleHostGatewayImage
+}
+
+func v1ToV2alpha1_GatewayConfigurerImage(v1 *v1.CheCluster, v2 *v2alpha1.CheCluster) {
+	v2.Spec.Gateway.ConfigurerImage = v1.Spec.Server.SingleHostGatewayConfigSidecarImage
+}
+
+func v1ToV2alpha1_TlsSecretName(v1 *v1.CheCluster, v2 *v2alpha1.CheCluster) {
+	// v1.Spec.Server.CheHostTLSSecret is used specifically for Che Host - i.e. the host under which che server is deployed.
+	// In DW we would only used that for subpath endpoints but wouldn't know what TLS to use for subdomain endpoints.
+	// Che server always uses the default cluster certificate for these on OpenShift, and the K8s.TlsSecretName on K8s.
+	// Because we're dealing with endpoints, let's try to use the secret on Kubernetes and nothing (e.g. the default cluster cert on OpenShift)
+	// which is in line with the logic of the Che server. Let's ignore CheHostTLSSecret for now - this is used only for che server which
+	// we are not exposing anyway.
+	if !util.IsOpenShift {
+		v2.Spec.TlsSecretName = v1.Spec.K8s.TlsSecretName
+	}
+}
+
+func v1ToV2alpha1_K8sIngressAnnotations(v1 *v1.CheCluster, v2 *v2alpha1.CheCluster) {
+	annotations := map[string]string{
+		"kubernetes.io/ingress.class":                       v1.Spec.K8s.IngressClass,
+		"nginx.ingress.kubernetes.io/proxy-read-timeout":    "3600",
+		"nginx.ingress.kubernetes.io/proxy-connect-timeout": "3600",
+		"nginx.ingress.kubernetes.io/ssl-redirect":          "true",
+	}
+	// This is what is applied in the deploy/ingress.go but I don't think it is applicable in our situation
+	// if ingressStrategy != "multi-host" && (component == DevfileRegistryName || component == PluginRegistryName) {
+	// 	annotations["nginx.ingress.kubernetes.io/rewrite-target"] = "/$1"
+	// }
+
+	v2.Spec.K8s.IngressAnnotations = annotations
+}
+
+func v2alpha1ToV1_Host(v1 *v1.CheCluster, v2 *v2alpha1.CheCluster) {
+	v1.Spec.Server.CheHost = v2.Spec.Gateway.Host
+}
+
+func v2alpha1ToV1_WorkspaceBaseDomain(v1 *v1.CheCluster, v2 *v2alpha1.CheCluster) {
+	if util.IsOpenShift {
+		if v1.Spec.Server.CustomCheProperties == nil {
+			v1.Spec.Server.CustomCheProperties = map[string]string{}
+		}
+		v1.Spec.Server.CustomCheProperties[routeDomainSuffixPropertyKey] = v2.Spec.WorkspaceBaseDomain
+	} else {
+		v1.Spec.K8s.IngressDomain = v2.Spec.WorkspaceBaseDomain
+	}
+}
+
+func v2alpha1ToV1_GatewayEnabled(v1 *v1.CheCluster, v2 *v2alpha1.CheCluster) {
+	v1Strategy := util.GetServerExposureStrategy(v1)
+	var v2Strategy string
+	if *v2.Spec.Gateway.Enabled {
+		v2Strategy = "single-host"
+	} else {
+		v2Strategy = "multi-host"
+	}
+
+	if v1Strategy != v2Strategy && v1Strategy != "default-host" {
+		// we need to reconstruct what the configuration might have looked like in the original
+		if v1.Spec.Server.ServerExposureStrategy == "" {
+			if util.IsOpenShift {
+				v1.Spec.Server.ServerExposureStrategy = v2Strategy
+			} else {
+				v1.Spec.K8s.IngressStrategy = v2Strategy
+			}
+		} else {
+			v1.Spec.Server.ServerExposureStrategy = v2Strategy
+		}
+	}
+}
+
+func v2alpha1ToV1_GatewayImage(v1 *v1.CheCluster, v2 *v2alpha1.CheCluster) {
+	v1.Spec.Server.SingleHostGatewayImage = v2.Spec.Gateway.Image
+}
+
+func v2alpha1ToV1_GatewayConfigurerImage(v1 *v1.CheCluster, v2 *v2alpha1.CheCluster) {
+	v1.Spec.Server.SingleHostGatewayConfigSidecarImage = v2.Spec.Gateway.Image
+}
+
+func v2alpha1ToV1_TlsSecretName(v1 *v1.CheCluster, v2 *v2alpha1.CheCluster) {
+	if !util.IsOpenShift {
+		v1.Spec.K8s.TlsSecretName = v2.Spec.TlsSecretName
+	}
+}
+
+func v2alpha1ToV1_K8sIngressAnnotations(v1 *v1.CheCluster, v2 *v2alpha1.CheCluster) {
+	ingressClass := v2.Spec.K8s.IngressAnnotations["kubernetes.io/ingress.class"]
+	if ingressClass == "" {
+		ingressClass = "nginx"
+	}
+	v1.Spec.K8s.IngressClass = ingressClass
+}
---
+++ pkg/apis/org/v2alpha1/checluster.go
@@ -0,0 +1,169 @@
+//
+// Copyright (c) 2012-2021 Red Hat, Inc.
+// This program and the accompanying materials are made
+// available under the terms of the Eclipse Public License 2.0
+// which is available at https://www.eclipse.org/legal/epl-2.0/
+//
+// SPDX-License-Identifier: EPL-2.0
+//
+// Contributors:
+//   Red Hat, Inc. - initial API and implementation
+//
+
+package v2alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// NOTE THAT THIS IS CURRENTLY INTENTIONALLY NOT PART OF THE GENERATED API
+//
+// (the generator comments are switched off by using a '\' instead of a '+')
+//
+// This is so that we can start using this spec in the code before we are
+// actually ready to start deploying it in the cluster.
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// CheClusterSpec holds the configuration of the Che controller.
+// \k8s:openapi-gen=true
+type CheClusterSpec struct {
+	// The workspace endpoints that need to be deployed on a subdomain will be deployed on subdomains of this base domain.
+	// This is mandatory on Kubernetes. On OpenShift, an attempt is made to automatically figure out the base domain of
+	// the routes. The resolved value of this property is written to the status.
+	WorkspaceBaseDomain string `json:"workspaceBaseDomain,omitempty"`
+
+	// Gateway contains the configuration of the gateway used for workspace endpoint routing.
+	Gateway CheGatewaySpec `json:"gateway,omitempty"`
+
+	// Name of a secret that will be used to setup ingress/route TLS certificate.
+	// When the field is empty string, the default cluster certificate will be used.
+	// The same secret is assumed to exist in the same namespace as the CheCluster CR and is used for both
+	// the gateway and all devworkspace endpoints.
+	// In case of the devworkspace endpoints, the secret is copied to the namespace of the devworkspace.
+	//
+	// The secret has to be of type "tls".
+	//
+	// +optional
+	TlsSecretName string `json:"tlsSecretName,omitempty"`
+
+	// K8s contains the configuration specific only to Kubernetes
+	K8s CheClusterSpecK8s `json:"k8s,omitempty"`
+}
+
+type CheGatewaySpec struct {
+	// Enabled enables or disables routing of the url rewrite supporting devworkspace endpoints
+	// through a common gateway (the hostname of which is defined by the Host).
+	//
+	// Default value is "true" meaning that the gateway is enabled.
+	//
+	// If set to true (i.e. the gateway is enabled), endpoints marked using the "urlRewriteSupported" attribute
+	// are exposed on unique subpaths of the Host, while the rest of the devworkspace endpoints are exposed
+	// on subdomains of the Host.
+	//
+	// If set to false (i.e. the gateway is disabled), all endpoints are deployed on subdomains of
+	// the Host.
+	Enabled *bool `json:"enabled,omitempty"`
+
+	// Host is the full host name used to expose devworkspace endpoints that support url rewriting reverse proxy.
+	// See the gateway.enabled attribute for a more detailed description of where and how are devworkspace endpoints
+	// exposed in various configurations.
+	//
+	// This attribute is mandatory on Kubernetes, optional on OpenShift.
+	Host string `json:"host,omitempty"`
+
+	// Image is the docker image to use for the Che gateway.  This is only used if Enabled is true.
+	// If not defined in the CR, it is taken from
+	// the `RELATED_IMAGE_gateway` environment variable of the operator deployment/pod. If not defined there,
+	// it defaults to a hardcoded value.
+	Image string `json:"image,omitempty"`
+
+	// ConfigurerImage is the docker image to use for the sidecar of the Che gateway that is
+	// used to configure it. This is only used when Enabled is true. If not defined in the CR,
+	// it is taken from the `RELATED_IMAGE_gateway_configurer` environment variable of the operator
+	// deployment/pod. If not defined there, it defaults to a hardcoded value.
+	ConfigurerImage string `json:"configurerImage,omitempty"`
+}
+
+// CheClusterSpecK8s contains the configuration options specific to Kubernetes only.
+type CheClusterSpecK8s struct {
+	// IngressAnnotations are the annotations to be put on the generated ingresses. This can be used to
+	// configure the ingress class and the ingress-controller-specific behavior for both the gateway
+	// and the ingresses created to expose the Devworkspace component endpoints.
+	// When not specified, this defaults to:
+	//
+	//     kubernetes.io/ingress.class:                       "nginx"
+	//     nginx.ingress.kubernetes.io/proxy-read-timeout:    "3600",
+	//     nginx.ingress.kubernetes.io/proxy-connect-timeout: "3600",
+	//     nginx.ingress.kubernetes.io/ssl-redirect:          "true"
+	//
+	// +optional
+	IngressAnnotations map[string]string `json:"ingressAnnotations,omitempty"`
+}
+
+// GatewayPhase describes the different phases of the Che gateway lifecycle
+type GatewayPhase string
+
+const (
+	GatewayPhaseInitializing = "Initializing"
+	GatewayPhaseEstablished  = "Established"
+	GatewayPhaseInactive     = "Inactive"
+)
+
+// ClusterPhase describes the different phases of the Che cluster lifecycle
+type ClusterPhase string
+
+const (
+	ManagerPhaseActive          = "Active"
+	ManagerPhaseInactive        = "Inactive"
+	ManagerPhasePendingDeletion = "PendingDeletion"
+)
+
+// CheClusterStatus contains the status of the CheCluster object
+// \k8s:openapi-gen=true
+type CheClusterStatus struct {
+	// GatewayPhase specifies the phase in which the gateway deployment currently is.
+	// If the gateway is disabled, the phase is "Inactive".
+	GatewayPhase GatewayPhase `json:"gatewayPhase,omitempty"`
+
+	// GatewayHost is the resolved host of the ingress/route. This is equal to the Host in the spec
+	// on Kubernetes but contains the actual host name of the route if Host is unspecified on OpenShift.
+	GatewayHost string `json:"gatewayHost,omitempty"`
+
+	// Phase is the phase in which the Che cluster as a whole finds itself in.
+	Phase ClusterPhase `json:"phase,omitempty"`
+
+	// Message contains further human-readable info for why the Che cluster is in the phase it currently is.
+	Message string `json:"message,omitempty"`
+
+	// The resolved workspace base domain. This is either the copy of the explicitly defined property of the
+	// same name in the spec or, if it is undefined in the spec and we're running on OpenShift, the automatically
+	// resolved basedomain for routes.
+	WorkspaceBaseDomain string `json:"workspaceBaseDomain,omitempty"`
+}
+
+// CheCluster is the configuration of the CheCluster layer of Devworkspace.
+// \k8s:openapi-gen=true
+// \kubebuilder:subresource:status
+// \kubebuilder:resource:path=checlusters,scope=Namespaced
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+type CheCluster struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   CheClusterSpec   `json:"spec,omitempty"`
+	Status CheClusterStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// CheClusterList is the list type for CheCluster
+type CheClusterList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []CheCluster `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&CheCluster{}, &CheClusterList{})
+}
