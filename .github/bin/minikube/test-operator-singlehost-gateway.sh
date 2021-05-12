fdc167373dd1cbcf6617fc9f64f2f3ae84552788
Flavius Lacatusu <flacatus@redhat.com>
2021-05-12 09:47:57 +0200

    MacOS install
    Signed-off-by: Flavius Lacatusu <flacatus@redhat.com>

--- .github/bin/minikube/test-operator-singlehost-gateway.sh
+++ .github/bin/minikube/test-operator-singlehost-gateway.sh
@@ -17,7 +17,6 @@ set -u
 # Get absolute path for root repo directory from github actions context: https://docs.github.com/en/free-pro-team@latest/actions/reference/context-and-expression-syntax-for-github-actions
 export OPERATOR_REPO="${GITHUB_WORKSPACE}"
 source "${OPERATOR_REPO}"/.github/bin/common.sh
-source "${OPERATOR_REPO}/olm/olm.sh"
 
 # Stop execution on any error
 trap "catchFinish" EXIT SIGINT
@@ -40,6 +39,5 @@ runTest() {
 initDefaults
 initLatestTemplates
 prepareTemplates
-buildCheOperatorImage
 copyCheOperatorImageToMinikube
 runTest
