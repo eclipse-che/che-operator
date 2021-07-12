e3ef568f0f37713677bbde546503c8162fb000ef
Anatolii Bazko <abazko@redhat.com>
2021-07-12 13:12:22 +0300

    fix: Fix setting image digests on release
    Signed-off-by: Anatolii Bazko <abazko@redhat.com>

--- olm/buildDigestMap.sh
+++ olm/buildDigestMap.sh
@@ -60,6 +60,9 @@ setImagesFromDeploymentEnv
 setOperatorImage
 echo "${OPERATOR_IMAGE}"
 
+setDevWorkspaceCheOperatorImage
+echo ${DEVWORKSPACE_CHE_OPERATOR_IMAGE}
+
 setPluginRegistryList
 echo "${PLUGIN_REGISTRY_LIST}"
 
@@ -89,6 +92,7 @@ writeDigest() {
     *)
       # for other build methods or for falling back to other registries when not found, can apply transforms here
       orig_image=${image}
+      echo "===========> $image "
       if [[ -x ${SCRIPTS_DIR}/buildDigestMapAlternateURLs.sh ]]; then
         # shellcheck source=buildDigestMapAlternateURLs.sh
         . ${SCRIPTS_DIR}/buildDigestMapAlternateURLs.sh
@@ -101,7 +105,7 @@ writeDigest() {
       digest="$(skopeo inspect --tls-verify=false docker://${image} 2>/dev/null | jq -r '.Digest')"
       fi
       if [[ -z ${digest} ]]; then
-        echo "==================== Failed to get digest for image: ${image}======================"
+        echo "Failed to get digest for image: ${image}"
         withoutTag=""
         withDigest=""
       else
@@ -126,9 +130,10 @@ rm -Rf "${DIGEST_FILE}"
 touch "${DIGEST_FILE}"
 
 writeDigest "${OPERATOR_IMAGE}" "operator-image"
+writeDigest "${DEVWORKSPACE_CHE_OPERATOR_IMAGE}" "devworkspace che operator-image"
 
 for image in ${REQUIRED_IMAGES}; do
-  writeDigest "${image}" "required-image" 
+  writeDigest "${image}" "required-image"
 done
 
 for image in ${PLUGIN_REGISTRY_LIST}; do
