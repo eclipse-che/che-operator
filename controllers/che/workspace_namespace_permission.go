f7a1f5b4080bbe8ab74ffb063a3607dedd79b9ac
Igor Vinokur <ivinokur@redhat.com>
2021-07-28 17:12:19 +0300

    fix: Add get and patch secret roles
    Signed-off-by: Igor Vinokur <ivinokur@redhat.com>

--- controllers/che/workspace_namespace_permission.go
+++ controllers/che/workspace_namespace_permission.go
@@ -261,7 +261,7 @@ func getWorkspacesPolicies() []rbac.PolicyRule {
 		{
 			APIGroups: []string{""},
 			Resources: []string{"secrets"},
-			Verbs:     []string{"list", "create", "delete"},
+			Verbs:     []string{"list", "create", "delete", "get", "patch"},
 		},
 		{
 			APIGroups: []string{""},
