2a867abd75ca478b7f0aa08b3861fabd56c3292d
Anatolii Bazko <abazko@redhat.com>
2021-08-02 16:49:13 +0300

    fix tests
    Signed-off-by: Anatolii Bazko <abazko@redhat.com>

--- .github/bin/minikube/test-olm.sh
+++ .github/bin/minikube/test-olm.sh
@@ -42,10 +42,13 @@ runTest() {
   sleep 10s
   createWorkspaceDevWorkspaceController
   waitAllPodsRunning ${DEVWORKSPACE_CONTROLLER_TEST_NAMESPACE}
+  kubectl delete namespace ${DEVWORKSPACE_CONTROLLER_TEST_NAMESPACE}
 
-  sleep 10s
+  sleep 60s
+
   createWorkspaceDevWorkspaceCheOperator
   waitAllPodsRunning ${DEVWORKSPACE_CHE_OPERATOR_TEST_NAMESPACE}
+  kubectl delete namespace ${DEVWORKSPACE_CHE_OPERATOR_TEST_NAMESPACE}
 }
 
 initDefaults
