62f7a25ee4ca095b0dbbfbbc4fa6418f23c44731
Anatolii Bazko <abazko@redhat.com>
2021-08-17 15:31:12 +0300

    fix: remove RELATED_IMAGE_devworkspace_che_operator env var (#1014)
    * fix: remove RELATED_IMAGE_devworkspace_che_operator env var
    * Fix setting com.redhat.openshift.versions label"
    
    Signed-off-by: Anatolii Bazko <abazko@redhat.com>

--- olm/release-olm-files.sh
+++ olm/release-olm-files.sh
@@ -171,8 +171,8 @@ do
 
   if [[ ${CHANNEL} == "stable-all-namespaces" ]]; then
     # Set specific OpenShift version
-    sed -e 's/# LABEL com.redhat.openshift.versions=/LABEL com.redhat.openshift.versions="v4.8"/' -i "${BUNDLE_DOCKERFILE}"
-    sed -e 's/# com.redhat.openshift.versions:/com.redhat.openshift.versions: "v4.8"/' -i "${ANNOTATION_METADATA_YAML}"
+    echo -e "\nLABEL com.redhat.openshift.versions=\"v4.8\"" >> "${BUNDLE_DOCKERFILE}"
+    echo -e "\n  com.redhat.openshift.versions: \"v4.8\"" >> "${ANNOTATION_METADATA_YAML}"
   fi
 
   exit 0
