eclipse-che/che-operator

commit f555494a7d5764cbed3e057b5bc092d411970610

feat: Newer operator sdk (#826)
* Define new dependencies. Update vendor folder. Use newer operator sdk v1.7.1.

Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

Dockerfile
@@ -16,8 +16,19 @@ ENV RESTIC_TAG=v0.12.0
 ARG DEV_WORKSPACE_CONTROLLER_VERSION="main"
 ARG DEV_WORKSPACE_CHE_OPERATOR_VERSION="main"
 USER root
-ADD . /che-operator
+
 WORKDIR /che-operator
+# Copy the Go Modules manifests
+COPY go.mod go.mod
+COPY go.sum go.sum
+
+# Copy the go source
+COPY main.go main.go
+COPY api/ api/
+COPY controllers/ controllers/
+COPY templates/ templates/
+COPY pkg/ pkg/
+COPY vendor/ vendor/
 
 # upstream, download zips for every build
 # downstream, copy prefetched asset-*.zip into /tmp
@@ -27,9 +38,7 @@ RUN curl -sSLo /tmp/asset-devworkspace-operator.zip https://api.github.com/repos
 
 # build operator
 RUN export ARCH="$(uname -m)" && if [[ ${ARCH} == "x86_64" ]]; then export ARCH="amd64"; elif [[ ${ARCH} == "aarch64" ]]; then export ARCH="arm64"; fi && \
-    export MOCK_API=true && \
-    go test -mod=vendor -v ./... && \
-    GOOS=linux GOARCH=${ARCH} CGO_ENABLED=0 go build -mod=vendor -o /tmp/che-operator/che-operator cmd/manager/main.go
+    CGO_ENABLED=0 GOOS=linux GOARCH=amd64 GO111MODULE=on go build -a -o che-operator main.go
 
 RUN unzip /tmp/asset-devworkspace-operator.zip */deploy/deployment/* -d /tmp && \
     mkdir -p /tmp/devworkspace-operator/templates/ && \
@@ -50,7 +59,7 @@ RUN mkdir -p $GOPATH/restic && cd $GOPATH/restic && \
 # https://access.redhat.com/containers/?tab=tags#/registry.access.redhat.com/ubi8-minimal
 FROM registry.access.redhat.com/ubi8-minimal:8.4-205
 
-COPY --from=builder /tmp/che-operator/che-operator /usr/local/bin/che-operator
+COPY --from=builder /che-operator/che-operator /manager
 COPY --from=builder /che-operator/templates/*.sh /tmp/
 COPY --from=builder /tmp/devworkspace-operator/templates/deploy /tmp/devworkspace-operator/templates
 COPY --from=builder /tmp/devworkspace-che-operator/templates/deploy /tmp/devworkspace-che-operator/templates
@@ -60,6 +69,10 @@ COPY --from=builder /go/restic/LICENSE /usr/local/bin/restic-LICENSE.txt
 # install httpd-tools for /usr/bin/htpasswd
 RUN microdnf install -y httpd-tools && microdnf -y update && microdnf -y clean all && rm -rf /var/cache/yum && echo "Installed Packages" && rpm -qa | sort -V && echo "End Of Installed Packages" && \
     mkdir ~/.ssh && chmod 0766  ~/.ssh
-CMD ["che-operator"]
+
+WORKDIR /
+USER 65532:65532
+
+ENTRYPOINT ["/manager"]
 
 # append Brew metadata here - see https://github.com/redhat-developer/codeready-workspaces-images/blob/crw-2-rhel-8/crw-jenkins/jobs/CRW_CI/crw-operator_2.x.jenkinsfile

build/Dockerfile
@@ -1,13 +1,0 @@
-# Copyright (c) 2018-2020 Red Hat, Inc.
-# This program and the accompanying materials are made
-# available under the terms of the Eclipse Public License 2.0
-# which is available at https://www.eclipse.org/legal/epl-2.0/
-#
-# SPDX-License-Identifier: EPL-2.0
-#
-# Contributors:
-#   Red Hat, Inc. - initial API and implementation
-#
-
-# Empty file.
-# Is required by operator-sdk for some tasks.

commit fb5b85fc77842d174de0fdd1b772bd17f9ca0100

fix: bug: make operator multiarch again - caused by https://github.com/eclipse-che/che-operator/pull/826/files#diff-dd2c0eb6ea5cfc6c4bd4eac30934e2d5746747af48fef6da689e85b752f39557R41
Change-Id: I4da98463e4223906e82372961de3c0b9e50ccbee
Signed-off-by: nickboldt <nboldt@redhat.com>

Dockerfile
@@ -45,7 +45,7 @@ COPY config/ config/
 RUN export ARCH="$(uname -m)" && if [[ ${ARCH} == "x86_64" ]]; then export ARCH="amd64"; elif [[ ${ARCH} == "aarch64" ]]; then export ARCH="arm64"; fi && \
     export MOCK_API=true && \
     go test -mod=vendor -v ./... && \
-    CGO_ENABLED=0 GOOS=linux GOARCH=amd64 GO111MODULE=on go build -mod=vendor -a -o che-operator main.go
+    CGO_ENABLED=0 GOOS=linux GOARCH=${ARCH} GO111MODULE=on go build -mod=vendor -a -o che-operator main.go
 
 RUN unzip /tmp/asset-devworkspace-operator.zip */deploy/deployment/* -d /tmp && \
     mkdir -p /tmp/devworkspace-operator/templates/ && \