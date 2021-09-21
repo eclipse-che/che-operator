4a88c2c9d6ca55a7332734365abd58adafd1904f
Lukas Krejci <lkrejci@redhat.com>
2021-09-21 14:33:08 +0200

    Adding license to the usernamespace files.

--- controllers/devworkspace/util.go
+++ controllers/devworkspace/util.go
@@ -1,3 +1,15 @@
+//
+// Copyright (c) 2019-2021 Red Hat, Inc.
+// This program and the accompanying materials are made
+// available under the terms of the Eclipse Public License 2.0
+// which is available at https://www.eclipse.org/legal/epl-2.0/
+//
+// SPDX-License-Identifier: EPL-2.0
+//
+// Contributors:
+//   Red Hat, Inc. - initial API and implementation
+//
+
 package devworkspace
 
 import (
@@ -11,8 +23,6 @@ const (
 	DevworkspaceStateNotPresent DevworkspaceState = 0
 	DevworkspaceStateDisabled   DevworkspaceState = 1
 	DevworkspaceStateEnabled    DevworkspaceState = 2
-	DevworkspaceStateDisabled   = 1
-	DevworkspaceStateEnabled    = 2
 )
 
 func GetDevworkspaceState(scheme *runtime.Scheme, cr *chev2alpha1.CheCluster) DevworkspaceState {
