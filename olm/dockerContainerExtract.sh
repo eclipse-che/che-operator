21da2fc695571f6d7d2675d5028c95dcf80157d5
Nick Boldt <nboldt@redhat.com>
2022-03-17 09:46:57 -0300

    fix: CRW-2818 switch to new flavour... (#1344)
    * fix: CRW-2818 switch to new flavour devspaces (no more codeready flavour)
    
    Change-Id: I5fd0ef785d55ca2cc0653c7afb696798c2f7935b
    Signed-off-by: nickboldt <nboldt@redhat.com>
    
    * run 'make update-resources -s'
    
    Change-Id: Ibe0bab0d3c803564feb161896f28a23bf378b444
    Signed-off-by: nickboldt <nboldt@redhat.com>
    
    * fixup
    
    Signed-off-by: Anatolii Bazko <abazko@redhat.com>
    
    Co-authored-by: Anatolii Bazko <abazko@redhat.com>

--- olm/dockerContainerExtract.sh
+++ olm/dockerContainerExtract.sh
@@ -15,9 +15,9 @@ set -e +x
 
 if [[ ! $1 ]]; then
   echo "Usage: $0 CONTAINER [tar-extraction-flags]"
-  echo "Usage: $0 quay.io/crw/operator-metadata:latest"
-  echo "Usage: $0 quay.io/crw/devfileregistry-rhel8:latest var/www/html/*/external_images.txt"
-  echo "Usage: $0 quay.io/crw/pluginregistry-rhel8:latest var/www/html/*/external_images.txt"
+  echo "Usage: $0 quay.io/devspaces/devspaces-operator-bundle:latest"
+  echo "Usage: $0 quay.io/devspaces/devfileregistry-rhel8:latest var/www/html/*/external_images.txt"
+  echo "Usage: $0 quay.io/devspaces/pluginregistry-rhel8:latest var/www/html/*/external_images.txt"
   exit
 fi
 
