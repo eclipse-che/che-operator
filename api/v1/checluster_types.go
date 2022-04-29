275bd600bbf087fac040ed26a5473ebc852f454e
nickboldt <nboldt@redhat.com>
2022-04-29 13:54:12 -0300

    fix: RHDEVDOCS-4042 (7.46.x)
    Change-Id: I2ab62eb8975507c37255c79386a0ea3a83f91c03
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- api/v1/checluster_types.go
+++ api/v1/checluster_types.go
@@ -99,8 +99,8 @@ type CheClusterSpecServer struct {
 	// +optional
 	CheImagePullPolicy corev1.PullPolicy `json:"cheImagePullPolicy,omitempty"`
 	// Deprecated. The value of this flag is ignored.
-	// Specifies a variation of the installation. The options are `che` for upstream Che installations, or `devspaces` for link:https://developers.redhat.com/products/openshift-dev-spaces/overview[Red Hat OpenShift Dev Spaces] installation.
-	// Override the default value only on necessary occasions.
+	// Specifies a variation of the installation. The options are `che` for upstream Che installations or
+	// `devspaces` for Red Hat OpenShift Dev Spaces (formerly Red Hat CodeReady Workspaces) installation
 	// +optional
 	CheFlavor string `json:"cheFlavor,omitempty"`
 	// Public host name of the installed Che server. When value is omitted, the value it will be automatically set by the Operator.
