f5070147e18411c205e722f710fddd25c5bc167f
Anatolii Bazko <abazko@redhat.com>
2022-07-25 10:15:49 +0300

    Update warning message
    Signed-off-by: Anatolii Bazko <abazko@redhat.com>

--- api/v1/checluster_types.go
+++ api/v1/checluster_types.go
@@ -876,7 +876,7 @@ type CheClusterStatus struct {
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
-// +kubebuilder:deprecatedversion:warning="org.eclipse.che/v1 CheCluster API is deprecated"
+// +kubebuilder:deprecatedversion:warning="org.eclipse.che/v1 CheCluster is deprecated and will be removed in future releases"
 // +k8s:openapi-gen=true
 // +operator-sdk:csv:customresourcedefinitions:displayName="Eclipse Che instance Specification"
 // +operator-sdk:csv:customresourcedefinitions:order=1
