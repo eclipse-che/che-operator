5118d606511b66687aaf5e223ae7bdce096a6f83
Anatolii Bazko <abazko@redhat.com>
2022-08-31 18:02:16 +0300

    feat: Add testCatalogFromSources.sh
    Signed-off-by: Anatolii Bazko <abazko@redhat.com>

--- build/scripts/oc-tests/oc-test-operator.sh
+++ build/scripts/oc-tests/oc-test-operator.sh
@@ -16,15 +16,13 @@ set -ex
 export OPERATOR_REPO=$(dirname "$(dirname "$(dirname "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")")")
 source "${OPERATOR_REPO}/build/scripts/oc-tests/oc-common.sh"
 
-#Stop execution on any error
+# Stop execution on any error
 trap "catchFinish" EXIT SIGINT
 
 [[ -z "${CI_CHE_OPERATOR_IMAGE}" ]] && { echo [ERROR] CI_CHE_OPERATOR_IMAGE not defined; exit 1; }
 
 runTests() {
-  deployEclipseCheFromSources
+  . ${OPERATOR_REPO}/build/scripts/olm/testCatalogFromSources -o ${CI_CHE_OPERATOR_IMAGE}
 }
 
 runTests
-
-
