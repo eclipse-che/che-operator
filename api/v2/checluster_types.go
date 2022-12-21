2bab80aabeca097d31a5388e3e391deb15756068
Anatolii Bazko <abazko@redhat.com>
2022-12-21 16:23:17 +0200

    feat: limit the number of workspaces per user
    Signed-off-by: Anatolii Bazko <abazko@redhat.com>

--- api/v2/checluster_types.go
+++ api/v2/checluster_types.go
@@ -130,6 +130,10 @@ type CheClusterDevEnvironments struct {
 	// +kubebuilder:validation:Minimum:=1
 	// +kubebuilder:default:=300
 	StartTimeoutSeconds *int32 `json:"startTimeoutSeconds,omitempty"`
+	// Total number of workspaces that a user can keep.
+	// The default value, `-1`, allows users to keep an unlimited number of workspaces.
+	// +kubebuilder:default:=-1
+	WorkspacesLimitPerUser *int32 `json:"workspacesLimitPerUser,omitempty"`
 }
 
 // Che components configuration.
