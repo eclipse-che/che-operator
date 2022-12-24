90314e52a38ef058b7a4cb26e35dab87d64bdada
Anatolii Bazko <abazko@redhat.com>
2022-12-24 10:34:12 +0200

    Update resources
    Signed-off-by: Anatolii Bazko <abazko@redhat.com>

--- api/v2/checluster_types.go
+++ api/v2/checluster_types.go
@@ -37,7 +37,7 @@ type CheClusterSpec struct {
 	// +optional
 	// +operator-sdk:csv:customresourcedefinitions:type=spec,order=1
 	// +operator-sdk:csv:customresourcedefinitions:type=spec,displayName="Development environments"
-	// +kubebuilder:default:={disableContainerBuildCapabilities: true, defaultComponents: {{name: universal-developer-image, container: {image: "quay.io/devfile/universal-developer-image:ubi8-38da5c2"}}}, defaultEditor: che-incubator/che-code/insiders, storage: {pvcStrategy: per-user}, defaultNamespace: {template: <username>-che, autoProvision: true}, secondsOfInactivityBeforeIdling:1800, secondsOfRunBeforeIdling:-1}
+	// +kubebuilder:default:={disableContainerBuildCapabilities: true, defaultComponents: {{name: universal-developer-image, container: {image: "quay.io/devfile/universal-developer-image:ubi8-38da5c2"}}}, defaultEditor: che-incubator/che-code/insiders, storage: {pvcStrategy: per-user}, defaultNamespace: {template: <username>-che, autoProvision: true}, secondsOfInactivityBeforeIdling:1800, secondsOfRunBeforeIdling:-1, startTimeoutSeconds:300}
 	DevEnvironments CheClusterDevEnvironments `json:"devEnvironments"`
 	// Che components configuration.
 	// +optional
@@ -126,6 +126,9 @@ type CheClusterDevEnvironments struct {
 	// StartTimeoutSeconds determines the maximum duration (in seconds) that a workspace can take to start
 	// before it is automatically failed.
 	// If not specified, the default value of 300 seconds (5 minutes) is used.
+	// +optional
+	// +kubebuilder:validation:Minimum:=1
+	// +kubebuilder:default:=300
 	StartTimeoutSeconds *int32 `json:"startTimeoutSeconds,omitempty"`
 }
 
