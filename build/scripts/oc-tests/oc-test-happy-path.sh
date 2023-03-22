b511a9ac08057b440ee292f3927a57fdad71704e
Maryna Dolhalova <72726250+nallikaea@users.noreply.github.com>
2023-03-22 14:42:04 +0100

    Updated oc-test-happy-path.sh according to new test scripts in che project #22043 (#1641)
    Signed-off-by: mdolhalo <mdolhalo@redhat.com>
    Co-authored-by: Maryna Dolhalova <mdolhalo@redhat.com>

--- build/scripts/oc-tests/oc-test-happy-path.sh
+++ build/scripts/oc-tests/oc-test-happy-path.sh
@@ -23,7 +23,8 @@ trap "catchFinish" EXIT SIGINT
 runTests() {
   . ${OPERATOR_REPO}/build/scripts/olm/test-catalog-from-sources.sh --verbose
 
-  export HAPPY_PATH_SUITE=test-empty-workspace-devworkspace-happy-path-code
+  export HAPPY_PATH_USERSTORY=EmptyWorkspace && export HAPPY_PATH_SUITE=test-all-devfiles
+
   bash <(curl -s ${DEVWORKSPACE_HAPPY_PATH}/remote-launch.sh)
 }
 
