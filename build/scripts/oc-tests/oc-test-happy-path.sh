dc8820bb7ac5003f80de42572ca1d392580264cc
Anatolii Bazko <abazko@redhat.com>
2023-03-28 16:09:09 +0300

    improvements
    Signed-off-by: Anatolii Bazko <abazko@redhat.com>

--- build/scripts/oc-tests/oc-test-happy-path.sh
+++ build/scripts/oc-tests/oc-test-happy-path.sh
@@ -21,7 +21,10 @@ source "${OPERATOR_REPO}/build/scripts/oc-tests/oc-common.sh"
 trap "catchFinish" EXIT SIGINT
 
 runTests() {
-  export HAPPY_PATH_USERSTORY=EmptyWorkspace && export HAPPY_PATH_SUITE=test-all-devfiles
+  . ${OPERATOR_REPO}/build/scripts/olm/test-catalog-from-sources.sh --verbose
+
+  export HAPPY_PATH_USERSTORY=EmptyWorkspace
+  export HAPPY_PATH_SUITE=test-all-devfiles
   bash <(curl -s ${DEVWORKSPACE_HAPPY_PATH}/remote-launch.sh)
 }
 
