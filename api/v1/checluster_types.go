507e4bd588ad2b54ca2eff35f0bce27603e26aa7
Valerii Svydenko <vsvydenk@redhat.com>
2023-03-29 12:18:42 +0300

    use UDI with ubi8-latest tag Replaces UDI:ubi8-38da5c2 with UDI:ubi8-latest to have latest changes form the universal developer image (UDI) in the default componen.
    Signed-off-by: Valerii Svydenko <vsvydenk@redhat.com>

--- api/v1/checluster_types.go
+++ api/v1/checluster_types.go
@@ -373,7 +373,6 @@ type CheClusterSpecServer struct {
 	// Default components applied to DevWorkspaces.
 	// These default components are meant to be used when a Devfile does not contain any components.
 	// +optional
-	// +kubebuilder:default:={{name: universal-developer-image, container: {image: "quay.io/devfile/universal-developer-image:ubi8-38da5c2"}}}
 	WorkspaceDefaultComponents []devfile.Component `json:"workspaceDefaultComponents,omitempty"`
 	// List of environment variables to set in the Che server container.
 	// +optional
