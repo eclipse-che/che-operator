bc7df1b5085f17cfc2ac97a4f954cc2cf2323192
Anatolii Bazko <abazko@redhat.com>
2023-04-25 14:07:57 +0300

    fix patch file
    Signed-off-by: Anatolii Bazko <abazko@redhat.com>

--- build/scripts/minikube-tests/common.sh
+++ build/scripts/minikube-tests/common.sh
@@ -224,8 +224,11 @@ EOF
 }
 
 startAndWaitDevWorkspace() {
+  # pre-pull image for faster workspace startup
+  minikube image pull quay.io/devfile/universal-developer-image:ubi8-latest
+
   kubectl patch devworkspace ${DEV_WORKSPACE_NAME} -p '{"spec":{"started":true}}' --type=merge -n ${USER_NAMESPACE}
-  kubectl wait devworkspace ${DEV_WORKSPACE_NAME} -n ${USER_NAMESPACE} --for=jsonpath='{.status.phase}'=Running --timeout=12000s
+  kubectl wait devworkspace ${DEV_WORKSPACE_NAME} -n ${USER_NAMESPACE} --for=jsonpath='{.status.phase}'=Running --timeout=300s
 }
 
 stopAndWaitDevWorkspace() {
