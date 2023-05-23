d55d8a77be22b185b5abd9dfbad0c2028ea9640a
Andrew Obuchowicz <aobuchow@redhat.com>
2023-05-23 18:13:40 +0000

    feat: add persistUserHome Che Cluster CR field
    Fix eclipse/che#22102
    
    Signed-off-by: Andrew Obuchowicz <aobuchow@redhat.com>

--- api/v2/checluster_types.go
+++ api/v2/checluster_types.go
@@ -85,6 +85,10 @@ type CheClusterDevEnvironments struct {
 	// +optional
 	// +kubebuilder:default:={pvcStrategy: per-user}
 	Storage WorkspaceStorage `json:"storage,omitempty"`
+	// PersistUserHome defines configuration options for persisting the
+	// user home directory in workspaces.
+	// +optional
+	PersistUserHome *PersistentHomeConfig `json:"persistUserHome,omitempty"`
 	// Default plug-ins applied to DevWorkspaces.
 	// +optional
 	DefaultPlugins []WorkspaceDefaultPlugins `json:"defaultPlugins,omitempty"`
@@ -435,6 +439,14 @@ type WorkspaceStorage struct {
 	// +kubebuilder:default:="per-user"
 	// +kubebuilder:validation:Enum=common;per-user;per-workspace;ephemeral
 	PvcStrategy string `json:"pvcStrategy,omitempty"`
+}
+
+type PersistentHomeConfig struct {
+    // Determines whether the user home directory in workspaces should persist between
+    // workspace shutdown and startup.
+    // Must be used with the 'per-user' or 'per-workspace' PVC strategy in order to take effect.
+    // Disabled by default.
+    Enabled *bool `json:"enabled,omitempty"`
 }
 
 type WorkspaceDefaultPlugins struct {
