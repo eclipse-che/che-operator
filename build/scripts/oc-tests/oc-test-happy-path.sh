cb48e7e3fdb050df395c541de85dabfd0419e8ed
Tibor Dancs <tdancs@redhat.com>
2023-10-15 22:05:45 +0200

    Updated PR-Check script to use SmokeTest
    Signed-off-by: Tibor Dancs <tdancs@redhat.com>

--- build/scripts/oc-tests/oc-test-happy-path.sh
+++ build/scripts/oc-tests/oc-test-happy-path.sh
@@ -23,8 +23,9 @@ trap "catchFinish" EXIT SIGINT
 runTests() {
   . ${OPERATOR_REPO}/build/scripts/olm/test-catalog-from-sources.sh --verbose
 
-  export HAPPY_PATH_USERSTORY=EmptyWorkspace
+  export HAPPY_PATH_USERSTORY=SmokeTest
   export HAPPY_PATH_SUITE=test
+  export MOCHA_DIRECTORY='.'
   bash <(curl -s ${DEVWORKSPACE_HAPPY_PATH}/remote-launch.sh)
 }
 
