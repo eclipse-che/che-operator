a4e19ead9d324dce09f5ca400a5cb7eff3abd253
Anatolii Bazko <abazko@redhat.com>
2023-11-02 15:16:31 +0100

    chore: Update dev resources (#1774)
    * chore: Update dev resources
    
    Signed-off-by: Anatolii Bazko <abazko@redhat.com>

--- pkg/deploy/gateway/traefik_config.go
+++ pkg/deploy/gateway/traefik_config.go
@@ -1,4 +1,5 @@
-// Copyright (c) 2019-2021 Red Hat, Inc.
+//
+// Copyright (c) 2019-2023 Red Hat, Inc.
 // This program and the accompanying materials are made
 // available under the terms of the Eclipse Public License 2.0
 // which is available at https://www.eclipse.org/legal/epl-2.0/
@@ -6,8 +7,9 @@
 // SPDX-License-Identifier: EPL-2.0
 //
 // Contributors:
+//   Red Hat, Inc. - initial API and implementation
 //
-//	Red Hat, Inc. - initial API and implementation
+
 package gateway
 
 // A representation of the Traefik config as we need it. This is in no way complete but can be used for the purposes we need it for.
--- pkg/deploy/identity-provider/identity_provider_util.go
+++ pkg/deploy/identity-provider/identity_provider_util.go
@@ -1,4 +1,5 @@
-// Copyright (c) 2019-2021 Red Hat, Inc.
+//
+// Copyright (c) 2019-2023 Red Hat, Inc.
 // This program and the accompanying materials are made
 // available under the terms of the Eclipse Public License 2.0
 // which is available at https://www.eclipse.org/legal/epl-2.0/
@@ -6,8 +7,9 @@
 // SPDX-License-Identifier: EPL-2.0
 //
 // Contributors:
+//   Red Hat, Inc. - initial API and implementation
 //
-//	Red Hat, Inc. - initial API and implementation
+
 package identityprovider
 
 import (
