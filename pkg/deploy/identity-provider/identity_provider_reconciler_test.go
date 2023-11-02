99705223e54da7739d46d975e4496cf8c700ffb3
Anatolii Bazko <abazko@redhat.com>
2023-11-02 14:36:59 +0100

    make fmt
    Signed-off-by: Anatolii Bazko <abazko@redhat.com>

--- pkg/deploy/identity-provider/identity_provider_reconciler_test.go
+++ pkg/deploy/identity-provider/identity_provider_reconciler_test.go
@@ -1,3 +1,4 @@
+//
 // Copyright (c) 2019-2023 Red Hat, Inc.
 // This program and the accompanying materials are made
 // available under the terms of the Eclipse Public License 2.0
@@ -6,8 +7,9 @@
 // SPDX-License-Identifier: EPL-2.0
 //
 // Contributors:
+//   Red Hat, Inc. - initial API and implementation
 //
-//	Red Hat, Inc. - initial API and implementation
+
 package identityprovider
 
 import (
