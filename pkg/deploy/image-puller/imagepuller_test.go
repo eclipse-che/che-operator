a6883ba7f88e6cd21a01277543b5aa03b9f6c024
Anatolii Bazko <abazko@redhat.com>
2024-03-27 14:15:32 +0100

    feat: Retrieve default images from plugin & devfile registries
    Signed-off-by: Anatolii Bazko <abazko@redhat.com>

--- pkg/deploy/image-puller/imagepuller_test.go
+++ pkg/deploy/image-puller/imagepuller_test.go
@@ -14,7 +14,6 @@ package imagepuller
 
 import (
 	"context"
-	"os"
 
 	chev1alpha1 "github.com/che-incubator/kubernetes-image-puller-operator/api/v1alpha1"
 	"github.com/eclipse-che/che-operator/pkg/deploy"
@@ -23,19 +22,13 @@ import (
 	"github.com/stretchr/testify/assert"
 	"k8s.io/apimachinery/pkg/api/errors"
 
+	chev2 "github.com/eclipse-che/che-operator/api/v2"
 	"github.com/eclipse-che/che-operator/pkg/common/constants"
 	"github.com/eclipse-che/che-operator/pkg/common/test"
-	"github.com/eclipse-che/che-operator/pkg/common/utils"
-
-	chev2 "github.com/eclipse-che/che-operator/api/v2"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	fakeDiscovery "k8s.io/client-go/discovery/fake"
-
-	"sigs.k8s.io/controller-runtime/pkg/client"
-	logf "sigs.k8s.io/controller-runtime/pkg/log"
-	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
 	"testing"
 )
@@ -48,15 +41,6 @@ func TestImagePullerConfiguration(t *testing.T) {
 		expectedImagePuller *chev1alpha1.KubernetesImagePuller
 	}
 
-	// unset RELATED_IMAGE environment variables, set them back after tests complete
-	matches := utils.GetGetArchitectureDependentEnvsByRegExp("^RELATED_IMAGE_.*")
-	for _, match := range matches {
-		if originalValue, exists := os.LookupEnv(match.Name); exists {
-			os.Unsetenv(match.Name)
-			defer os.Setenv(match.Name, originalValue)
-		}
-	}
-
 	testCases := []testCase{
 		{
 			name: "case #1: KubernetesImagePuller with defaults",
@@ -67,7 +51,6 @@ func TestImagePullerConfiguration(t *testing.T) {
 				DeploymentName:   defaultDeploymentName,
 				ConfigMapName:    defaultConfigMapName,
 				ImagePullerImage: defaultImagePullerImage,
-				Images:           getDefaultImages(),
 			}),
 		},
 		{
@@ -97,14 +80,12 @@ func TestImagePullerConfiguration(t *testing.T) {
 					DeploymentName:   defaultDeploymentName,
 					ConfigMapName:    defaultConfigMapName,
 					ImagePullerImage: defaultImagePullerImage,
-					Images:           getDefaultImages(),
 				}),
 			},
 			expectedImagePuller: InitImagePuller(chev1alpha1.KubernetesImagePullerSpec{
 				DeploymentName:   defaultDeploymentName,
 				ConfigMapName:    defaultConfigMapName,
 				ImagePullerImage: defaultImagePullerImage,
-				Images:           getDefaultImages(),
 			}),
 		},
 		{
@@ -122,7 +103,6 @@ func TestImagePullerConfiguration(t *testing.T) {
 					DeploymentName:   defaultDeploymentName,
 					ConfigMapName:    defaultConfigMapName,
 					ImagePullerImage: defaultImagePullerImage,
-					Images:           getDefaultImages(),
 				}),
 			},
 			expectedImagePuller: InitImagePuller(chev1alpha1.KubernetesImagePullerSpec{
@@ -142,7 +122,6 @@ func TestImagePullerConfiguration(t *testing.T) {
 					DeploymentName:   defaultDeploymentName,
 					ConfigMapName:    defaultConfigMapName,
 					ImagePullerImage: defaultImagePullerImage,
-					Images:           getDefaultImages(),
 				}),
 			},
 		},
@@ -150,9 +129,7 @@ func TestImagePullerConfiguration(t *testing.T) {
 
 	for _, testCase := range testCases {
 		t.Run(testCase.name, func(t *testing.T) {
-			logf.SetLogger(zap.New(zap.WriteTo(os.Stdout), zap.UseDevMode(true)))
-
-			ctx := test.GetDeployContext(testCase.cheCluster, []runtime.Object{})
+			ctx := test.GetDeployContext(testCase.cheCluster, testCase.initObjects)
 			ctx.ClusterAPI.DiscoveryClient.(*fakeDiscovery.FakeDiscovery).Fake.Resources = []*metav1.APIResourceList{
 				{
 					GroupVersion: "che.eclipse.org/v1alpha1",
@@ -162,11 +139,6 @@ func TestImagePullerConfiguration(t *testing.T) {
 						},
 					},
 				},
-			}
-
-			for _, obj := range testCase.initObjects {
-				err := ctx.ClusterAPI.Client.Create(context.TODO(), obj.(client.Object))
-				assert.NoError(t, err)
 			}
 
 			ip := NewImagePuller()
@@ -187,56 +159,6 @@ func TestImagePullerConfiguration(t *testing.T) {
 				}
 			} else {
 				assert.True(t, errors.IsNotFound(err))
-			}
-		})
-	}
-}
-
-func TestDefaultImages(t *testing.T) {
-	type testcase struct {
-		name                   string
-		env                    map[string]string
-		expected               string
-		expectedImagesAsString string
-	}
-
-	// unset RELATED_IMAGE environment variables, set them back after tests complete
-	matches := utils.GetGetArchitectureDependentEnvsByRegExp("^RELATED_IMAGE_.*")
-	for _, match := range matches {
-		if originalValue, exists := os.LookupEnv(match.Name); exists {
-			os.Unsetenv(match.Name)
-			defer os.Setenv(match.Name, originalValue)
-		}
-	}
-
-	cases := []testcase{
-		{
-			name: "case #1",
-			env: map[string]string{
-				"RELATED_IMAGE_che_theia_plugin_registry_image_IBZWQYJ":                         "quay.io/eclipse/che-theia",
-				"RELATED_IMAGE_che_theia_endpoint_runtime_binary_plugin_registry_image_IBZWQYJ": "quay.io/eclipse/che-theia-endpoint-runtime-binary",
-			},
-			expected: "che-theia-endpoint-runtime-binary-plugin-registry-image-ibzwqyj=quay.io/eclipse/che-theia-endpoint-runtime-binary;che-theia-plugin-registry-image-ibzwqyj=quay.io/eclipse/che-theia;",
-		},
-		{
-			name: "case #2",
-			env: map[string]string{
-				"RELATED_IMAGE_che_machine_exec_plugin_registry_image_IBZWQYJ":                  "quay.io/eclipse/che-machine-exec",
-				"RELATED_IMAGE_codeready_workspaces_machineexec_plugin_registry_image_GIXDCMQK": "registry.redhat.io/codeready-workspaces/machineexec-rhel8",
-			},
-			expected: "che-machine-exec-plugin-registry-image-ibzwqyj=quay.io/eclipse/che-machine-exec;codeready-workspaces-machineexec-plugin-registry-image-gixdcmqk=registry.redhat.io/codeready-workspaces/machineexec-rhel8;",
-		},
-	}
-
-	for _, c := range cases {
-		t.Run(c.name, func(t *testing.T) {
-			for k, v := range c.env {
-				os.Setenv(k, v)
-				defer os.Unsetenv(k)
-			}
-			actual := getDefaultImages()
-			if d := cmp.Diff(c.expected, actual); d != "" {
-				t.Errorf("Error, collected images differ (-want, +got): %v", d)
 			}
 		})
 	}
