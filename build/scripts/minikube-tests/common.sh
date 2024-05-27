e0aae8b7be98db8b8fe694da8446422823a959c6
Anatolii Bazko <abazko@redhat.com>
2024-05-27 11:18:08 +0200

    fixup
    Signed-off-by: Anatolii Bazko <abazko@redhat.com>

--- build/scripts/minikube-tests/common.sh
+++ build/scripts/minikube-tests/common.sh
@@ -311,9 +311,6 @@ spec:
     name: editor
   routingClass: che
   started: false
-  contributions:
-    - name: ide
-      uri: http://plugin-registry.eclipse-che.svc:8080/v3/plugins/che-incubator/che-code/insiders/devfile.yaml
   template:
     attributes:
       controller.devfile.io/storage-type: ephemeral
