1325073f312688e215130a9b544f9ef550687b21
Maxime <maxleriche.60@gmail.Com>
2024-07-23 22:28:47 +0200

    Update che_routing.go
    Signed-off-by: Max batleforc <maxleriche.60@gmail.com>

--- controllers/devworkspace/solver/che_routing.go
+++ controllers/devworkspace/solver/che_routing.go
@@ -630,6 +630,7 @@ func add5XXErrorHandling(cfg *gateway.TraefikConfig, dwId string) {
 			DialTimeout: "2500ms",
 		},
 	}
+	// TODO: Add retry for the health endpoint with a little more time between
 	cfg.AddRetry(dwId, 2, "500ms")
 	cfg.HTTP.Services[dwId].LoadBalancer.ServersTransport = dwId
 }
