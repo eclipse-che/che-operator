c86b004308d559626790235454a1153b3c43d29f
Anatolii Bazko <abazko@redhat.com>
2026-03-11 16:06:35 +0100

    fixes
    Signed-off-by: Anatolii Bazko <abazko@redhat.com>

--- pkg/deploy/server/basedomain_reconciler.go
+++ pkg/deploy/server/basedomain_reconciler.go
@@ -13,19 +13,14 @@
 package server
 
 import (
-	"context"
 	"fmt"
 	"strings"
 
 	"github.com/devfile/devworkspace-operator/pkg/infrastructure"
 	"github.com/eclipse-che/che-operator/pkg/common/chetypes"
-	defaults "github.com/eclipse-che/che-operator/pkg/common/operator-defaults"
 	"github.com/eclipse-che/che-operator/pkg/common/reconciler"
 	"github.com/eclipse-che/che-operator/pkg/common/utils"
 	"github.com/eclipse-che/che-operator/pkg/deploy"
-	routev1 "github.com/openshift/api/route/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
@@ -45,12 +40,11 @@ func (r *BaseDomainReconciler) Reconcile(ctx *chetypes.DeployContext) (reconcile
 
 	if baseDomain == "" {
 		if infrastructure.IsOpenShift() {
-			openshiftBaseDomain, err := r.detectOpenShiftRouteBaseDomain(ctx)
-			if err != nil {
-				return reconcile.Result{}, false, err
+			items := strings.SplitAfterN(ctx.CheHost, ".", 2)
+			if len(items) != 2 {
+				return reconcile.Result{}, false, fmt.Errorf("unable to detect base domain")
 			}
-
-			baseDomain = openshiftBaseDomain
+			baseDomain = items[1]
 		}
 	}
 
@@ -70,36 +64,4 @@ func (r *BaseDomainReconciler) Reconcile(ctx *chetypes.DeployContext) (reconcile
 
 func (r *BaseDomainReconciler) Finalize(ctx *chetypes.DeployContext) bool {
 	return true
-}
-
-// Tries to autodetect the route base domain.
-func (r *BaseDomainReconciler) detectOpenShiftRouteBaseDomain(ctx *chetypes.DeployContext) (string, error) {
-	name := "devworkspace-che-test"
-	testRoute := &routev1.Route{
-		ObjectMeta: metav1.ObjectMeta{
-			Namespace: ctx.CheCluster.Namespace,
-			Name:      name,
-			Labels:    deploy.GetLabels(defaults.GetCheFlavor()),
-		},
-		Spec: routev1.RouteSpec{
-			To: routev1.RouteTargetReference{
-				Kind: "Service",
-				Name: name,
-			},
-		},
-	}
-
-	if err := ctx.ClusterAPI.ClientWrapper.Create(context.TODO(), testRoute); err != nil {
-		return "", err
-	}
-
-	defer func() {
-		_ = ctx.ClusterAPI.ClientWrapper.DeleteByKeyIgnoreNotFound(
-			context.TODO(),
-			types.NamespacedName{Name: testRoute.Name, Namespace: testRoute.Namespace},
-			&routev1.Route{})
-	}()
-
-	items := strings.SplitAfterN(testRoute.Spec.Host, ".", 2)
-	return items[1], nil
 }
---
+++ pkg/deploy/server/basedomain_reconciler_test.go
@@ -0,0 +1,150 @@
+//
+// Copyright (c) 2019-2026 Red Hat, Inc.
+// This program and the accompanying materials are made
+// available under the terms of the Eclipse Public License 2.0
+// which is available at https://www.eclipse.org/legal/epl-2.0/
+//
+// SPDX-License-Identifier: EPL-2.0
+//
+// Contributors:
+//   Red Hat, Inc. - initial API and implementation
+//
+
+package server
+
+import (
+	"context"
+	"testing"
+
+	chev2 "github.com/eclipse-che/che-operator/api/v2"
+	"github.com/eclipse-che/che-operator/pkg/common/test"
+	"github.com/stretchr/testify/assert"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+func TestBaseDomainFromNetworkingDomain(t *testing.T) {
+	ctx := test.NewCtxBuilder().WithCheCluster(&chev2.CheCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "eclipse-che",
+			Name:      "eclipse-che",
+		},
+		Spec: chev2.CheClusterSpec{
+			Networking: chev2.CheClusterSpecNetworking{
+				Domain: "my-domain.com",
+			},
+		},
+	}).Build()
+
+	reconciler := NewBaseDomainReconciler()
+	_, done, err := reconciler.Reconcile(ctx)
+
+	assert.True(t, done)
+	assert.Nil(t, err)
+	assert.Equal(t, "my-domain.com", ctx.CheCluster.Status.WorkspaceBaseDomain)
+}
+
+func TestBaseDomainFromExtraProperties(t *testing.T) {
+	ctx := test.NewCtxBuilder().WithCheCluster(&chev2.CheCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "eclipse-che",
+			Name:      "eclipse-che",
+		},
+		Spec: chev2.CheClusterSpec{
+			Networking: chev2.CheClusterSpecNetworking{
+				Domain: "default-domain.com",
+			},
+			Components: chev2.CheClusterComponents{
+				CheServer: chev2.CheServer{
+					ExtraProperties: map[string]string{
+						"CHE_INFRA_OPENSHIFT_ROUTE_HOST_DOMAIN__SUFFIX": "custom-domain.com",
+					},
+				},
+			},
+		},
+	}).Build()
+
+	reconciler := NewBaseDomainReconciler()
+	_, done, err := reconciler.Reconcile(ctx)
+
+	assert.True(t, done)
+	assert.Nil(t, err)
+	assert.Equal(t, "custom-domain.com", ctx.CheCluster.Status.WorkspaceBaseDomain)
+}
+
+func TestBaseDomainExtraPropertiesOverridesNetworkingDomain(t *testing.T) {
+	ctx := test.NewCtxBuilder().WithCheCluster(&chev2.CheCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "eclipse-che",
+			Name:      "eclipse-che",
+		},
+		Spec: chev2.CheClusterSpec{
+			Networking: chev2.CheClusterSpecNetworking{
+				Domain: "networking-domain.com",
+			},
+			Components: chev2.CheClusterComponents{
+				CheServer: chev2.CheServer{
+					ExtraProperties: map[string]string{
+						"CHE_INFRA_OPENSHIFT_ROUTE_HOST_DOMAIN__SUFFIX": "extra-domain.com",
+					},
+				},
+			},
+		},
+	}).Build()
+
+	reconciler := NewBaseDomainReconciler()
+	_, done, err := reconciler.Reconcile(ctx)
+
+	assert.True(t, done)
+	assert.Nil(t, err)
+	assert.Equal(t, "extra-domain.com", ctx.CheCluster.Status.WorkspaceBaseDomain)
+}
+
+func TestBaseDomainStatusUpdated(t *testing.T) {
+	ctx := test.NewCtxBuilder().WithCheCluster(&chev2.CheCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "eclipse-che",
+			Name:      "eclipse-che",
+		},
+		Spec: chev2.CheClusterSpec{
+			Networking: chev2.CheClusterSpecNetworking{
+				Domain: "new-domain.com",
+			},
+		},
+		Status: chev2.CheClusterStatus{
+			WorkspaceBaseDomain: "old-domain.com",
+		},
+	}).Build()
+
+	reconciler := NewBaseDomainReconciler()
+	_, done, err := reconciler.Reconcile(ctx)
+
+	assert.True(t, done)
+	assert.Nil(t, err)
+	assert.Equal(t, "new-domain.com", ctx.CheCluster.Status.WorkspaceBaseDomain)
+
+	// Verify status was persisted
+	cheCluster := &chev2.CheCluster{}
+	err = ctx.ClusterAPI.Client.Get(context.TODO(), types.NamespacedName{Name: "eclipse-che", Namespace: "eclipse-che"}, cheCluster)
+	assert.Nil(t, err)
+	assert.Equal(t, "new-domain.com", cheCluster.Status.WorkspaceBaseDomain)
+}
+
+func TestBaseDomainFromCheHost(t *testing.T) {
+	ctx := test.NewCtxBuilder().WithCheCluster(&chev2.CheCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "eclipse-che",
+			Name:      "eclipse-che",
+		},
+		Status: chev2.CheClusterStatus{
+			CheURL: "https://che.apps.cluster.example.com",
+		},
+	}).Build()
+
+	reconciler := NewBaseDomainReconciler()
+	_, done, err := reconciler.Reconcile(ctx)
+
+	assert.True(t, done)
+	assert.Nil(t, err)
+	assert.Equal(t, "apps.cluster.example.com", ctx.CheCluster.Status.WorkspaceBaseDomain)
+}
